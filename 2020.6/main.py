d31948a5bef0a77921e8f3701faaf5bdfa2a0cb5
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2020-12-07 12:12:28 +0100

    Add day six.

---
+++ 2020.6/main.py
@@ -0,0 +1,41 @@
+import numpy as np
+import re
+import pathlib
+from functools import reduce
+from string import ascii_lowercase
+
+DIR = pathlib.Path(__file__).parent.absolute()
+
+
+def read():
+    with open(DIR / "input.txt") as f:
+        t = f.read().replace("\r", "").split("\n\n")
+    t = [l.split("\n") for l in t]
+    if t[-1] == "":
+        t.pop()
+    if t[-1][-1] == "":
+        t[-1].pop()
+
+    return t
+
+
+def easy():
+    t = read()
+
+    t = [len(reduce(lambda a, b: a | set(list(b)), e, set())) for e in t]
+    print(sum(t))
+
+
+def hard():
+    t = read()
+
+    t = [
+        len(reduce(lambda a, b: a & set(list(b)), e, set(list(ascii_lowercase))))
+        for e in t
+    ]
+    print(sum(t))
+
+
+if __name__ == "__main__":
+    easy()
+    hard()
