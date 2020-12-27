f5b4e2653445de823846e8658d13fe13b180909a
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2020-12-28 00:18:43 +0100

    Make generic.

--- 2020.17/main.py
+++ 2020.17/main.py
@@ -39,42 +39,26 @@ def step(A):
     return A_
 
 
-def easy():
-    A = np.zeros((N, N, N), dtype=np.int32)
-    A[N // 2 - 4 : N // 2 + 4, N // 2 - 4 : N // 2 + 4, N // 2] = t
+def do_stuff(dim):
+    A = np.zeros((N,) * dim, dtype=np.int32)
+    index = [
+        N // 2,
+    ] * dim
+    for i in [0, 1]:
+        index[i] = slice(N // 2 - 4, N // 2 + 4, None)
+    A[tuple(index)] = t
 
-    # fancyprint(A)
     for _ in range(6):
         A = step(A)
-        # fancyprint(A)
     print(A.sum())
+
+
+def easy():
+    do_stuff(3)
 
 
 def hard():
-    A = np.zeros((N, N, N, N), dtype=np.int32)
-    A[N // 2 - 4 : N // 2 + 4, N // 2 - 4 : N // 2 + 4, N // 2, N // 2] = t
-
-    # fancyprint(A)
-    for _ in range(6):
-        A = step(A)
-        # fancyprint(A)
-    print(A.sum())
-
-
-def fancyprint(A):
-    for z in range(N):
-        if A[:, :, z].sum() == 0:
-            continue
-        print("z = ", z - N // 2)
-        d = np.array(np.where(A[:, :, z] > 0))
-        d = abs(d.reshape(d.size) - N // 2).max()
-        A_ = A[N // 2 - d : N // 2 + d, N // 2 - d : N // 2 + d, z]
-        for row in A_:
-            for e in row:
-                print("#" if e == 1 else ".", end="")
-            print()
-        print()
-    print("---")
+    do_stuff(4)
 
 
 if __name__ == "__main__":
