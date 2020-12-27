50d17a50e42802e19916ee08d7acd9ec27b4d791
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2020-12-27 15:08:21 +0100

    Add day twenty five part one.

---
+++ 2020.25/main.py
@@ -0,0 +1,52 @@
+import numpy as np
+import re
+import pathlib
+import json
+from functools import reduce
+from string import ascii_lowercase
+from math import prod
+from itertools import permutations, product
+
+DIR = pathlib.Path(__file__).parent.absolute()
+inf = float("inf")
+
+
+def read():
+    with open(DIR / "input.txt") as f:
+        t = f.read().replace("\r", "").split("\n")
+    if t[-1] == "":
+        t.pop()
+    return [int(i) for i in t]
+
+
+t = read()
+
+
+def find_loop(pub, subject=7):
+    k = i = 1
+    while k != pub:
+        k = encrypt(k, subject, 1)
+        i += 1
+    return i - 1
+
+
+def encrypt(value, subject, loop):
+    for _ in range(loop):
+        value = (value * subject) % 20201227
+    return value
+
+
+def easy():
+    loop_card = find_loop(t[0])
+    print(loop_card)
+    loop_door = find_loop(t[1])
+    print(encrypt(1, t[0], loop_door))
+
+
+def hard():
+    return
+
+
+if __name__ == "__main__":
+    easy()
+    hard()
