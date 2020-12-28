5b6597c1b6bd95afb68ca9babf4a533be0b76651
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2020-12-28 21:13:31 +0100

    Add part two.

--- 2020.18/main.py
+++ 2020.18/main.py
@@ -14,11 +14,68 @@ inf = float("inf")
 def read():
     with open(DIR / "input.txt") as f:
         t = f.read().replace("\r", "").split("\n")
-    t.pop()
-    return [l.split(" ") for l in t]
+    return t[:-1]
 
 
 t = read()
+
+
+class Expression:
+    def __init__(self, line, parent=None, parent_index=None):
+        self.parent = parent
+        self.parent_index = parent_index
+        bracket = 0
+        sub_indices = []
+        for i, c in enumerate(line):
+            if c == "(":
+                bracket += 1
+                if bracket == 1:
+                    sub_indices.append([i, 0])
+            if c == ")":
+                bracket -= 1
+                if bracket == 0:
+                    sub_indices[-1][1] = i
+        self.children = []
+        for i, s_e in enumerate(sub_indices):
+            sub = line[s_e[0] + 1 : s_e[1]]
+            self.children.append(Expression(sub, self, i))
+        i = len(sub_indices)
+        for s, e in reversed(sub_indices):
+            i -= 1
+            line = line[:s] + "$%d$" % i + line[e + 1 :]
+        self.resolved = False
+        self.line = line
+
+    def calc(self):
+        line = self.line.split(" ")
+        while len(line) > 1:
+            has_plus = "+" in line
+            for i, op in enumerate(line):
+                if op == "+":
+                    line[i - 1] = int(line[i - 1]) + int(line[i + 1])
+                elif not has_plus and op == "*":
+                    line[i - 1] = int(line[i - 1]) * int(line[i + 1])
+                else:
+                    continue
+                del line[i]
+                del line[i]
+                break
+        return int(line[0])
+
+    def resolve(self):
+        if self.resolved:
+            return self.value
+        self.resolved = True
+        for c in self.children:
+            c.resolve()
+
+        self.value = self.calc()
+
+        if self.parent is not None:
+            self.parent.line = self.parent.line.replace(
+                "$%d$" % self.parent_index, str(self.value)
+            )
+        return self.value
 
 
 def calc(line):
@@ -44,16 +101,15 @@ def calc(line):
             line.insert(0, "0" + ")" * (op.count(")") - 1))
             line.insert(0, "+")
             return result
-
     return result
 
 
 def easy():
-    print(sum([calc(i) for i in t]))
+    print(sum([calc(i.split(" ")) for i in t]))
 
 
 def hard():
-    return
+    print(sum([Expression(i).resolve() for i in t]))
 
 
 if __name__ == "__main__":
