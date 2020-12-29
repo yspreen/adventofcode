ec9f279823f70d79a8bc57d5515571a66de3a9a2
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2020-12-29 15:54:02 +0100

    Add part two.

--- 2020.22/main.py
+++ 2020.22/main.py
@@ -31,18 +31,41 @@ def round():
         t[1].extend([p2, p1])
 
 
+def recround(a, b, return_list=False):
+    seen = set()
+    while True:
+        if not (a and b):
+            if return_list:
+                return a if a else b
+            return 0 if a else 1
+        h = hash(tuple(a + [-1] + b))
+        if h in seen:
+            return a if return_list else 0
+        seen.add(h)
+
+        p1 = a.pop(0)
+        p2 = b.pop(0)
+        if p1 <= len(a) and p2 <= len(b):
+            win = recround(list(a[:p1]), list(b[:p2]))
+        else:
+            win = 0 if p1 > p2 else 1
+        if win == 0:
+            a.extend([p1, p2])
+        else:
+            b.extend([p2, p1])
+
+
+def score(A):
+    return reduce(lambda a, b: (a[1] * b + a[0], a[1] + 1), reversed(A), (0, 1))[0]
+
+
 def easy():
-    while len(t[0]) and len(t[1]):
-        round()
-    print(
-        reduce(lambda a, b: (a[1] * b + a[0], a[1] + 1), reversed(t[0] + t[1]), (0, 1))[
-            0
-        ]
-    )
+    round()
+    print(score(t[0] + t[1]))
 
 
 def hard():
-    return
+    print(score(recround(*read(), return_list=True)))
 
 
 if __name__ == "__main__":
