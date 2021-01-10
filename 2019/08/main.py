05e7d5184fd93d878705f0f3a2fb8fcce36dfb5d
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-01-10 18:16:43 +0100

    Add day eight part one of 2019.

---
+++ 2019/08/main.py
@@ -0,0 +1,40 @@
+import numpy as np
+import re
+import pathlib
+import json
+from functools import reduce
+from string import ascii_lowercase
+from math import prod
+from itertools import permutations, product
+
+DIR = pathlib.Path(__file__).parent.absolute()
+inf = float("inf")
+
+
+def read():
+    with open(DIR / "input.txt") as f:
+        t = f.read().split("\n")
+    if t[-1] == "":
+        t.pop()
+    return np.array([int(i) for i in t[0]], dtype=np.int32).reshape(25, 6, -1)
+
+
+t = read()
+
+
+def easy():
+    m = (inf, 0)
+    for i in range(100):
+        c = len(np.where(t[:, :, i] == 0)[0])
+        if c < m[0]:
+            m = (c, i)
+    print(prod([len(np.where(t[:, :, m[1]] == k)[0]) for k in range(1, 3)]))
+
+
+def hard():
+    return
+
+
+if __name__ == "__main__":
+    easy()
+    hard()
