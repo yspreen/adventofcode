d341d3fad7527690ff521eac8dabfa167bc6410f
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-01-10 17:33:26 +0100

    Merge solutions.

--- 2019/07/main.py
+++ 2019/07/main.py
@@ -11,96 +11,129 @@ DIR = pathlib.Path(__file__).parent.absolute()
 inf = float("inf")
 
 
-def read():
-    with open(DIR / "input.txt") as f:
-        t = f.read().replace("\n", "").split(",")
-    if t[-1] == "":
-        t.pop()
-    return [int(i) for i in t]
-
-
-t = read()
-
-
 def digit(num, dig):
     dig = 10 ** dig
     return (num // dig) % 10
 
 
-inputs = outputs = None
+STOP = -1.337
 
 
-def op(i):
-    global t, inputs, outputs
+class VM:
+    def read(self):
+        with open(DIR / "input.txt") as f:
+            t = f.read().replace("\n", "").split(",")
+        if t[-1] == "":
+            t.pop()
+        self.t = [int(i) for i in t]
 
-    code = t[i]
-    ins = code % 100
-    mode_a = digit(code, 2)
-    mode_b = digit(code, 3)
-    mode_c = digit(code, 4)
+    def op(self, i):
+        code = self.t[i]
+        ins = code % 100
+        mode_a = digit(code, 2)
+        mode_b = digit(code, 3)
+        mode_c = digit(code, 4)
 
-    try:
-        a = t[i + 1]
-        b = t[i + 2]
-        c = t[i + 3]
-        if mode_a == 0 and ins != 3:
-            a = t[a]
-        if mode_b == 0:
-            b = t[b]
-    except:
-        # end of t.
-        pass
+        try:
+            a = self.t[i + 1]
+            if mode_a == 0 and ins != 3:
+                a = self.t[a]
+            b = self.t[i + 2]
+            if mode_b == 0:
+                b = self.t[b]
+            c = self.t[i + 3]
+        except:
+            # end of t.
+            pass
 
-    if ins == 99:
-        return 0
-    if ins == 1:
-        t[c] = a + b
-        return 4
-    if ins == 2:
-        t[c] = a * b
-        return 4
-    if ins == 3:
-        t[a] = inputs.pop(0)
-        return 2
-    if ins == 4:
-        outputs.append(a)
-        # print(a)
-        return 2
-    if ins == 5:
-        if a != 0:
-            return b - i
-        return 3
-    if ins == 6:
-        if a == 0:
-            return b - i
-        return 3
-    if ins == 7:
-        t[c] = 1 if a < b else 0
-        return 4
-    if ins == 8:
-        t[c] = 1 if a == b else 0
-        return 4
+        if ins == 99:
+            self.done = True
+            return STOP
+        if ins == 1:
+            self.t[c] = a + b
+            return 4
+        if ins == 2:
+            self.t[c] = a * b
+            return 4
+        if ins == 3:
+            if self.loop_mode and not self.inputs:
+                return STOP
+            self.t[a] = self.inputs.pop(0)
+            return 2
+        if ins == 4:
+            self.outputs.append(a)
+            return 2
+        if ins == 5:
+            if a != 0:
+                return b - i
+            return 3
+        if ins == 6:
+            if a == 0:
+                return b - i
+            return 3
+        if ins == 7:
+            self.t[c] = 1 if a < b else 0
+            return 4
+        if ins == 8:
+            self.t[c] = 1 if a == b else 0
+            return 4
+
+    def calc(self):
+        if self.done:
+            return []
+        self.d = 0
+        self.outputs = []
+        while self.d != STOP:
+            self.i += self.d
+            self.d = self.op(self.i)
+        return self.outputs
+
+    def __init__(self, *inp):
+        self.read()
+        self.outputs = []
+        self.inputs = list(inp)
+
+        self.i = self.d = 0
+        self.loop_mode = False
+        self.done = False
 
 
-def calc(*inp):
-    global t, inputs, outputs
-    t = read()
-
-    outputs = []
-    inputs = list(inp)
-
-    i = -1
-    d = 1
-    while d != 0:
-        i += d
-        d = op(i)
-    return outputs
+class LoopVM(VM):
+    def __init__(self, *inp):
+        super().__init__(*inp)
+        self.loop_mode = True
 
 
 def amp(*param):
+    vms = [LoopVM(p) for p in param]
+    vms[0].inputs.append(0)
+    n = len(param)
+    i = 0
+    E = []
+    while True:
+        a = vms[i].calc()
+        if i + 1 == n:
+            E.extend(a)
+            if vms[i].done:
+                return E[-1]
+        i += 1
+        i %= n
+        vms[i].inputs.extend(a)
+
+
+def hard():
+    m = (0, 0)
+    for p in permutations(range(5, 10)):
+        a = amp(*p)
+        if a > m[0]:
+            m = (a, p)
+    print(*m)
+
+
+def easy_amp(*param):
     a = 0
     for p in param:
-        a = calc(p, a)[0]
+        a = VM(p, a).calc()[0]
     return a
 
 
@@ -111,12 +144,7 @@ def easy():
         if a > m[0]:
             m = (a, p)
     print(*m)
-
-
-def hard():
-    from parttwo import hard
-
-    hard()
+    return
 
 
 if __name__ == "__main__":
