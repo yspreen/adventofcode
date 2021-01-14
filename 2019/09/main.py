6a22fa4484f3037ef2866996a3638d49fe0f905e
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-01-14 14:43:41 +0100

    Add day nine part one of 2019.

---
+++ 2019/09/main.py
@@ -0,0 +1,114 @@
+import numpy as np
+import re
+import pathlib
+import json
+from functools import reduce
+from string import ascii_lowercase
+from math import prod
+from itertools import permutations, product
+
+DIR = pathlib.Path(__file__).parent.absolute()
+inf = float("inf")
+
+
+def digit(num, dig):
+    dig = 10 ** dig
+    return (num // dig) % 10
+
+
+class VM:
+    def read(self):
+        with open(DIR / "input.txt") as f:
+            t = f.read().replace("\n", "").split(",")
+        if t[-1] == "":
+            t.pop()
+        t = [int(i) for i in t]
+        self.t = {i: v for i, v in enumerate(t)}
+
+    def op(self, i):
+        code = self.t[i]
+        ins = code % 100
+        mode_a = digit(code, 2)
+        mode_b = digit(code, 3)
+        mode_c = digit(code, 4)
+
+        a = self.t.get(i + 1, 0)
+        if mode_a == 2:
+            a += self.r
+            mode_a = 0
+        if mode_a == 0 and ins != 3:
+            a = self.t.get(a, 0)
+        b = self.t.get(i + 2, 0)
+        if mode_b == 0:
+            b = self.t.get(b, 0)
+        if mode_b == 2:
+            b = self.t.get(self.r + b, 0)
+        c = self.t.get(i + 3, 0)
+        if mode_c == 2:
+            c += self.r
+
+        if ins == 99:
+            self.done = True
+            return
+        if ins == 1:
+            self.t[c] = a + b
+            return 4
+        if ins == 2:
+            self.t[c] = a * b
+            return 4
+        if ins == 3:
+            if not self.inputs:
+                return
+            self.t[a] = self.inputs.pop(0)
+            return 2
+        if ins == 4:
+            self.outputs.append(a)
+            return 2
+        if ins == 5:
+            if a != 0:
+                return b - i
+            return 3
+        if ins == 6:
+            if a == 0:
+                return b - i
+            return 3
+        if ins == 7:
+            self.t[c] = 1 if a < b else 0
+            return 4
+        if ins == 8:
+            self.t[c] = 1 if a == b else 0
+            return 4
+        if ins == 9:
+            self.r += a
+            return 2
+
+    def calc(self):
+        if self.done:
+            return []
+        self.d = 0
+        self.outputs = []
+        while self.d is not None:
+            self.i += self.d
+            self.d = self.op(self.i)
+        return self.outputs
+
+    def __init__(self, *inp):
+        self.read()
+        self.outputs = []
+        self.inputs = list(inp)
+
+        self.i = self.d = self.r = 0
+        self.done = False
+
+
+def easy():
+    print(VM(1).calc())
+
+
+def hard():
+    return
+
+
+if __name__ == "__main__":
+    easy()
+    hard()
