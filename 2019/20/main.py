93161024922c543c375729a56933e5eed7676d50
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-01-24 00:07:28 +0100

    Not quite.

--- 2019/20/main.py
+++ 2019/20/main.py
@@ -15,28 +15,55 @@ def read():
     return np.array(t, np.int32)
 
 
-def neighbors(pos):
+class Move:
+    def __init__(self, newpos, level=0):
+        self.x = x
+        self.y = y
+        self.level = level
+
+
+def neigh_3d(pos):
+    n = []
+    pos, lvl = (pos[0], pos[1]), pos[2]
+    for p, c, dl in neigh[pos]:
+        if lvl + dl < 0 or p == -1:
+            continue
+        if p == -2:
+            if lvl + dl != 0:
+                continue
+            p = (-1, -1)
+        n.append(((p[0], p[1], lvl + dl), c))
+    return n
+
+
+def neighbors(pos, portals):
+    if len(pos) == 3:
+        return neigh_3d(pos)
     n = []
     for d in directions:
-        n.append((pos[0] + d[0], pos[1] + d[1]))
+        n.append(((pos[0] + d[0], pos[1] + d[1]), 1))
     try:
-        n.append(portal_dest[pos])
+        assert portals
+        n.append((portal_dest[pos], 1))
     except:
         pass
     return n
 
 
-def bfs(pos):
+def bfs(pos, portals=True):
     cost = {pos: 0}
     visited = set([pos])
     goal = [pos]
 
     while goal:
         pos = goal.pop(0)
-        for n in neighbors(pos):
-            if n in visited or t[n] != 0:
+        for n, c in neighbors(pos, portals=portals):
+            if n in visited or (len(n) == 2 and t[n] != 0):
                 continue
-            cost[n] = cost[pos] + 1
+            cost[n] = cost[pos] + c
+            if n == (-1, -1, 0):
+                goal = []
+                break
             goal.append(n)
             visited.add(n)
     return cost
@@ -61,18 +88,12 @@ def label_ord(x, y):
 
 
 def easy():
+    global points, portals, cost, portal_dest
     for a, b in [(32, 2), (35, 1), (46, 0)]:
         t[t == a] = b
-    # B = np.zeros_like(t, np.int32)
-    # for x, y in zip(*np.where(t > 64)):
-    #     B[tuple(label_pos(x, y))] = 1
-    #     B[(x, y)] = label_ord(x, y)
-    # for r in B:
-    #     for e in r:
-    #         print(" " if e == 0 else e, end="")
-    #     print()
     points = {}
     portals = {}
+    portal_dest = {}
     for x, y in zip(*np.where(t > 64)):
         point = label_pos(x, y)
         c = (t[x, y] - 65) * label_ord(x, y)
@@ -87,10 +108,27 @@ def easy():
     portal_dest[-2] = list(portals[26 * 26 - 1])[0]
     cost = bfs(portal_dest[-1])
     print(cost[portal_dest[-2]])
+    portal_dest[list(portals[0])[0]] = -1
+    portal_dest[list(portals[26 * 26 - 1])[0]] = -2
+
+
+def inner_donut(x, y):
+    return X / 2 + 2 < x < NX - X / 2 + 2 and Y / 2 + 2 < y < NY - Y / 2 + 2
 
 
 def hard():
-    return
+    for p in points.keys():
+        cost = bfs(p, portals=False)
+        neigh[p] = []
+        for b in [i for i in points.keys() if i != p]:
+            c = cost.get(b, None)
+            if not c:
+                continue
+            b_ = portal_dest[b]
+            neigh[p].append((b_, c + 1, 1 if inner_donut(*b) else -1))
+    # print(neigh)
+    cost = bfs((*portal_dest[-1], 0))
+    print(cost[(-1, -1, 0)])
 
 
 DIR = pathlib.Path(__file__).parent.absolute()
@@ -98,13 +136,13 @@ inf = float("inf")
 t = read()
 X, Y = [i[0] for i in np.where(t[2:-2, 2:-2] == 32)]
 NX, NY = t.shape[0] - 4, t.shape[1] - 4
-portal_dest = {}
 directions = (
     (-1, 0),  # u / N
     (0, 1),  # r / E
     (1, 0),  # d / S
     (0, -1),  # l / W
 )
+points = portals = cost = portal_dest = neigh = {}
 
 if __name__ == "__main__":
     easy()
