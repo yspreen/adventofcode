528f9eec8467fba949c6ef3c58691415b2d6f9bb
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-02-05 16:51:45 +0100

    Clean up day twelve.

--- 2018/12/12.py
+++ 2018/12/12.py
@@ -1,85 +1,28 @@
-input = """
-initial state: #..#.#..##......###...###
+import pathlib
 
-...## => #
-..#.. => #
-.#... => #
-.#.#. => #
-.#.## => #
-.##.. => #
-.#### => #
-#.#.# => #
-#.### => #
-##.#. => #
-##.## => #
-###.. => #
-###.# => #
-####. => #
-"""
-input = """
-initial state: ##..#.#.#..##..#..##..##..#.#....#.....##.#########...#.#..#..#....#.###.###....#..........###.#.#..
-
-..##. => .
-..... => .
-##..# => .
-...#. => .
-#.... => .
-...## => #
-.#.#. => .
-#..#. => #
-##.#. => .
-#..## => .
-..#.. => .
-#.#.# => .
-###.# => .
-###.. => .
-.#... => #
-.##.# => .
-##... => #
-..### => .
-####. => .
-#...# => #
-.#..# => #
-##### => #
-..#.# => #
-.#.## => #
-#.### => .
-....# => .
-.###. => .
-.#### => #
-.##.. => .
-##.## => #
-#.##. => #
-#.#.. => #
-"""
-
-input = input.split("\n")
-input = list(filter(None, input))
+input = open(pathlib.Path(__file__).parent / "input.txt").read().splitlines()
 
 state = input[0].split(" ")[-1]
-input = input[1:]
-rules = {i.split(" ")[0]: i.split(" ")[-1] for i in input}
-rule_len = 0
-for r in rules:
-    rule_len = len(r)
-    break
+rules = {i.split(" ")[0]: i.split(" ")[-1] for i in input[2:]}
+rule_len = len(list(rules.keys())[0])
 
-def main():
+
+def easy():
     global rules, state, rule_len
     rule_len_side = rule_len // 2
     prefix = "".join(["." for _ in range(rule_len_side)])
     shift = 0
     iterations = 20
-    
+
     for i in range(iterations):
-        if i % 10000 == 0:
-            print(100 * i / iterations)
+        # if i % 10000 == 0:
+        #     print(100 * i / iterations)
         new_state = ""
 
-        if state[0] == '#':
+        if state[0] == "#":
             state = prefix + state
             shift += 2
-        if state[-1] == '#':
+        if state[-1] == "#":
             state = state + prefix
         state = prefix + state + prefix
         while len(state) >= rule_len:
@@ -90,8 +33,48 @@ def main():
 
     s = 0
     for i in range(len(state)):
-        if state[i] == '#':
+        if state[i] == "#":
             s += i - shift
     print(s)
-    
-main()
+
+
+easy()
+
+state = input[0].split(" ")[-1]
+
+
+def hard():
+    global rules, state, rule_len
+    rule_len_side = rule_len // 2
+    prefix = "".join(["." for _ in range(rule_len_side)])
+    shift = 0
+    iterations = 1500
+    last_s = 0
+
+    for i in range(iterations):
+        new_state = ""
+
+        if state[0] == "#":
+            state = prefix + state
+            shift += 2
+        if state[-1] == "#":
+            state = state + prefix
+        state = prefix + state + prefix
+        while len(state) >= rule_len:
+            new_state += rules.get(state[0:rule_len], ".")
+            state = state[1:]
+
+        state = new_state
+        if i > 1000:
+            if i > 1100:
+                break
+            s = 0
+            for j in range(len(state)):
+                if state[j] == "#":
+                    s += j - shift
+            # print(i + 1, s, s - last_s)
+            last_s = s
+    print((50000000000 - i) * 23 + last_s)
+
+
+hard()
