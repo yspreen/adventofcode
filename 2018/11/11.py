52c4b006f3b3289131bda3f6af30ea2bb11137f0
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-02-05 16:40:48 +0100

    Clean up day eleven.

--- 2018/11/11.py
+++ 2018/11/11.py
@@ -1,9 +1,12 @@
-serial = 7139
-serial %= 1000
+import pathlib
+
+serial = open(pathlib.Path(__file__).parent / "input.txt").read().splitlines()[0]
+serial = int(serial) % 1000
+
 
 def main():
     global serial
-    
+
     arr = [[0 for _ in range(300)] for _ in range(300)]
 
     rack = 10
@@ -13,21 +16,26 @@ def main():
         for y in range(300):
             level += rack
             arr[x][y] = (rack * (level + serial)) // 100 % 10 - 5
-    
+
     sums = {}
-    for size in range(10, 21):
+    for size in [3] + list(range(10, 21)):
         for x in range(300 - size + 1):
             for y in range(300 - size + 1):
                 s = 0
-                for col in arr[x:(x+size)]:
-                    for el in col[y:(y+size)]:
+                for col in arr[x : (x + size)]:
+                    for el in col[y : (y + size)]:
                         s += el
-                sums["%d,%d,%d" % (x+1,y+1,size)] = s
-    max = 0
-    max_ind = 0
+                sums["%d,%d,%d" % (x + 1, y + 1, size)] = s
+    max3 = max = max_ind = max_ind3 = 0
     for ind in sums:
         if sums[ind] > max:
             max = sums[ind]
             max_ind = ind
-    print(max, max_ind)
+        if sums[ind] > max3 and ind[-2:] == ",3":
+            max3 = sums[ind]
+            max_ind3 = ind
+    print(max_ind3[:-2])
+    print(max_ind)
+
+
 main()
