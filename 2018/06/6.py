2ce60669a850c86ee561a9ecb0e68729281021ca
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-02-05 16:05:05 +0100

    Clean up day six.

--- 2018/06/6.py
+++ 2018/06/6.py
@@ -1,67 +1,10 @@
-input = [
-'78, 335',
-'74, 309',
-'277, 44',
-'178, 286',
-'239, 252',
-'118, 354',
-'170, 152',
-'75, 317',
-'156, 318',
-'172, 45',
-'138, 162',
-'261, 195',
-'306, 102',
-'282, 67',
-'53, 141',
-'191, 237',
-'352, 180',
-'95, 247',
-'353, 357',
-'201, 327',
-'316, 336',
-'57, 43',
-'119, 288',
-'299, 328',
-'125, 327',
-'187, 186',
-'121, 151',
-'121, 201',
-'43, 67',
-'76, 166',
-'238, 148',
-'326, 221',
-'219, 207',
-'237, 160',
-'345, 244',
-'321, 346',
-'48, 114',
-'304, 80',
-'265, 216',
-'191, 92',
-'54, 75',
-'118, 260',
-'336, 249',
-'81, 103',
-'290, 215',
-'300, 246',
-'293, 59',
-'150, 274',
-'296, 311',
-'264, 286',
-]
+import pathlib
 
-# input=[
-# '1, 1',
-# '1, 6',
-# '8, 3',
-# '3, 4',
-# '5, 5',
-# '8, 9',
-# ]
+input = open(pathlib.Path(__file__).parent / "input.txt").read().splitlines()
+input = [[int(i.split(", ")[0]), int(i.split(", ")[1])] for i in input]
 
-input = [[int(i.split(', ')[0]), int(i.split(', ')[1])] for i in input]
-def main():
+
+def easy():
     global input
     from contextlib import suppress
 
@@ -111,7 +54,9 @@ def main():
     max_x = max([i[0] for i in input]) + 1
     max_y = max([i[1] for i in input]) + 1
 
-    arr = [([[-1, max_x + max_y] for _ in range(max_y)] + [0]) for _ in range(max_x)] + [([0 for _ in range(max_y)] + [0])]
+    arr = [
+        ([[-1, max_x + max_y] for _ in range(max_y)] + [0]) for _ in range(max_x)
+    ] + [([0 for _ in range(max_y)] + [0])]
 
     queue = input
     while len(queue):
@@ -148,35 +93,31 @@ def main():
                     m = [0, m[1]]
             arr[i[0]][i[1]] = m
         with suppress(Exception):
-            p = [i[0]+1, i[1]]
+            p = [i[0] + 1, i[1]]
             if arr[p[0]][p[1]][0] == -1 and p not in queue:
                 queue.append(p)
         with suppress(Exception):
-            p = [i[0]-1, i[1]]
+            p = [i[0] - 1, i[1]]
             if arr[p[0]][p[1]][0] == -1 and p not in queue:
                 queue.append(p)
         with suppress(Exception):
-            p = [i[0], i[1]+1]
+            p = [i[0], i[1] + 1]
             if arr[p[0]][p[1]][0] == -1 and p not in queue:
                 queue.append(p)
         with suppress(Exception):
-            p = [i[0], i[1]-1]
+            p = [i[0], i[1] - 1]
             if arr[p[0]][p[1]][0] == -1 and p not in queue:
                 queue.append(p)
 
     import string
-    chars = '.' + string.ascii_lowercase + string.ascii_uppercase
+
+    chars = "." + string.ascii_lowercase + string.ascii_uppercase
 
     out = ""
     for y in range(max_y):
         for x in range(max_x):
             out += chars[arr[x][y][0]]
         out += "\n"
-
-    # with open("out2.tmp", "w") as f:
-    #     f.write(out)
-    with open("out.tmp", "w") as f:
-        f.write(out)
 
     m = [0]
 
@@ -186,23 +127,45 @@ def main():
         if c > m[0]:
             m = [c, n, chars[n]]
 
-    print(m)
-    #> [4887, 40, 'N']
+    print(m[0])
 
 
-main()
+easy()
 
 
-""" out2.tmp
+maxd = 10000
 
-aaaa.ccc
-aaddeccc
-adddeccc
-.dddeecc
-b.deeeec
-bb.eeee.
-bb.eeeff
-bb.eefff
-bb.fffff
+min_x = min([i[0] for i in input])
+min_y = min([i[1] for i in input])
 
-"""
+for i in input:
+    i[0] -= min_x
+    i[1] -= min_y
+
+max_x = max([i[0] for i in input]) + 1
+max_y = max([i[1] for i in input]) + 1
+
+
+def hard():
+    global input
+    from contextlib import suppress
+
+    arr = [
+        ([[-1, max_x + max_y] for _ in range(max_y)] + [0]) for _ in range(max_x)
+    ] + [([0 for _ in range(max_y)] + [0])]
+
+    c = 0
+    for x in range(max_x):
+        for y in range(max_y):
+            d = 0
+            for i in input:
+                d += abs(i[0] - x)
+                d += abs(i[1] - y)
+                if d > maxd:
+                    break
+            if d < maxd:
+                c += 1
+    print(c)
+
+
+hard()
