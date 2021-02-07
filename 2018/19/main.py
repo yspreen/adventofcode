66d856f3566a3198f093502dd04cf66cc5ff657d
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-02-07 13:47:58 +0100

    Fix number.

--- 2018/19/main.py
+++ 2018/19/main.py
@@ -16,7 +16,7 @@ def read():
     return t[0], [[i[:4], *map(int, i[5:].split(" "))] for i in t[1:]]
 
 
-def op(o, i):
+def op(o):
     code, A, B, C = o
 
     if code == "addr":
@@ -51,49 +51,23 @@ def op(o, i):
         s = "[%d] = [%d] == %d" % (C, A, B)
     if code == "eqrr":
         s = "[%d] = [%d] == [%d]" % (C, A, B)
-    s = re.sub(r"\[%d\]" % ip, "$P", s)
-    s = re.sub(r"\[0\]", "$0", s)
-    s = re.sub(r"\[1\]", "$a", s)
-    s = re.sub(r"\[2\]", "$b", s)
-    s = re.sub(r"\[3\]", "$c", s)
-    s = re.sub(r"\[4\]", "$d", s)
-    s = re.sub(r"\[5\]", "$e", s)
-    s = re.sub(r"\$P = \$P \+ \$(.)", r"SKIP if $\1", s)
-    s = re.sub(r"\$P = \$(.) \+ \$P", r"SKIP if $\1", s)
-    if "=" in s:
-        s = s.split("=")
-        s[1] = s[1].replace("$P", str(i))
-        if re.match(r"^(\s|\+|\d)+$", s[1]):
-            s[1] = " " + str(
-                sum(map(int, filter(None, s[1].replace("+", "0").split(" "))))
-            )
-        if re.match(r"^(\s|\*|\d)+$", s[1]):
-            s[1] = " " + str(
-                prod(map(int, filter(None, s[1].replace("*", "1").split(" "))))
-            )
-        s = "=".join(s)
-    s = re.sub(r"(.+) = (.+) \+ \1", r"\1 += \2", s)
-    s = re.sub(r"(.+) = \1 \+ (.+)", r"\1 += \2", s)
-    s = re.sub(r"(.+) = (.+) \* \1", r"\1 *= \2", s)
-    s = re.sub(r"(.+) = \1 \* (.+)", r"\1 *= \2", s)
-    s = re.sub(r"\$P = (\d+)", r"GOTO \1", s)
-    if "GOTO" in s:
-        s = "GOTO " + str(int(s[5:]) + 1)
-    print("%3d: %s" % (i, s))
+    print(s)
+
+
+def fact(N):
+    n = 0
+    for a in range(N):
+        if N % (a + 1) == 0:
+            n += a + 1
+    print(n)
 
 
 def easy():
-    while not op():
-        continue
-    print(reg[0])
+    fact(945)
 
 
 def hard():
-    global reg
-
-    reg = [0] * 6
-    reg[0] = 1
-    easy()
+    fact(10551345)
 
 
 DIR = pathlib.Path(__file__).parent.absolute()
@@ -104,6 +78,5 @@ reg = [0] * 6
 
 
 if __name__ == "__main__":
-    # print("#ip %d" % ip)
-    for i, o in enumerate(t):
-        op(o, i)
+    easy()
+    hard()
