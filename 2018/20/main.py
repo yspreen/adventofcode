bab121e71961f1c63be9fe69419e3eba117953ba
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-02-13 23:23:27 +0100

    Add test cases.

--- 2018/20/main.py
+++ 2018/20/main.py
@@ -18,6 +18,7 @@ def read():
 
 class Segment:
     unresolved = []
+    items = []
 
     @classmethod
     def resolve(cls):
@@ -29,6 +30,8 @@ class Segment:
     def __init__(self, s):
         self.choices = []
         self.children = []
+        self.items.append(self)
+        self.id = len(self.items)
         segments = [-1]
         splits = [-1]
         brace_num = 0
@@ -77,8 +80,13 @@ class Segment:
             return s[0]
         return self.s
 
+    lengths_ = {}
+
     @property
     def lengths(self):
+        l = Segment.lengths_.get(self.id, None)
+        if l is not None:
+            return l
         if self.children:
             c = [i.lengths for i in self.children]
             a = c[0]
@@ -87,12 +95,35 @@ class Segment:
                 for i in range(M + 1):
                     a[i] = np.roll(a[i], i)
                 a = np.sum(a, 0)
-            return a
-        if self.choices:
-            return np.sum([s.lengths for s in self.choices], axis=0)
-        A = np.zeros(M + 1)
-        A[self.s] = 1
-        return A
+            l = a
+        elif self.choices:
+            l = np.sum([s.lengths for s in self.choices], axis=0)
+        else:
+            A = np.zeros(M + 1, np.uint32)
+            A[self.s] = 1
+            l = A
+        Segment.lengths_[self.id] = l
+        return l
+
+
+def combine(a, b):
+    a = np.outer(a, b)
+    for i in range(M + 1):
+        a[i] = np.roll(a[i], i)
+    return np.sum(a, 0)
+
+
+def combine_reference(a, b):
+    a = [[i] * j for i, j in enumerate(a) if j > 0]
+    b = [[i] * j for i, j in enumerate(b) if j > 0]
+    a = [i for sublist in a for i in sublist]
+    b = [i for sublist in b for i in sublist]
+
+    r = np.zeros(M + 1, np.uint32)
+
+    for i, j in product(a, b):
+        r[i + j] += 1
+    return r
 
 
 def shorten(s):
@@ -116,7 +147,9 @@ def easy():
 
 
 def hard():
-    print(t.lengths)
+    for i in reversed(Segment.items):
+        i.lengths
+    print(list(t.lengths))
 
 
 DIR = pathlib.Path(__file__).parent.absolute()
@@ -125,5 +158,14 @@ t = read()
 Segment.resolve()
 
 if __name__ == "__main__":
-    easy()
-    hard()
+    M = 10
+    a = np.zeros(M + 1, np.uint32)
+    a[2] = 3
+    a[4] = 5
+    b = np.zeros(M + 1, np.uint32)
+    b[5] = 1
+    b[6] = 4
+    print(a)
+    print(b)
+    print(combine_reference(a, b))
+    print(combine(a, b))
