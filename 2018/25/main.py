34358c87abaa81af249fd882815b96ecf5c26cb2
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-03-07 21:53:31 +0100

    Add 25 part 1.

---
+++ 2018/25/main.py
@@ -0,0 +1,60 @@
+import numpy as np
+import re
+import pathlib
+import json
+from functools import reduce
+from string import ascii_lowercase
+from math import prod, gcd
+from itertools import permutations, product
+from multiprocessing import Pool
+
+
+class Cluster:
+    def __init__(self, p):
+        self.points = set([p])
+
+
+class Pos:
+    items = []
+
+    def dist(self, other):
+        return sum(map(lambda i: abs(other.p[i] - self.p[i]), range(4)))
+
+    def __init__(self, s):
+        self.p = list(map(int, s.split(",")))
+        self.cluster = Cluster(self)
+        merge = []
+        for i in self.items:
+            if i.dist(self) <= 3:
+                merge.append(i)
+        self.items.append(self)
+        for i in merge:
+            self.cluster.points |= i.cluster.points
+            for p in i.cluster.points:
+                p.cluster = self.cluster
+
+
+def read():
+    with open(DIR / "input") as f:
+        t = f.read().splitlines()
+    for r in t:
+        Pos(r)
+
+
+def easy():
+    s = set()
+    for i in Pos.items:
+        s |= {i.cluster}
+    print(len(s))
+
+
+def hard():
+    return
+
+
+DIR = pathlib.Path(__file__).parent.absolute()
+inf = float("inf")
+read()
+if __name__ == "__main__":
+    easy()
+    hard()
