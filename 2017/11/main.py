704084caa205d998c9b4e70db5bebbf48d99f90e
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-03-14 23:51:40 +0100

    Add day 11.

---
+++ 2017/11/main.py
@@ -0,0 +1,96 @@
+import numpy as np
+import re
+import pathlib
+import json
+from functools import reduce
+from string import ascii_lowercase
+from math import prod, gcd, sqrt
+from itertools import permutations, product
+
+
+def lmap(*a):
+    return list(map(*a))
+
+
+def read():
+    with open(DIR / "input") as f:
+        s = f.read() if teststr == "" else teststr
+    return lmap(str, s.splitlines()[0].split(","))
+
+
+def reduce():
+    global t
+    replaced = True
+    while replaced:
+        replaced = False
+        for k, v in {"ne": "sw", "nw": "se"}.items():
+            try:
+                a = t.index(k)
+                b = t.index(v)
+                del t[max(a, b)]
+                del t[min(a, b)]
+                replaced = 1
+            except:
+                pass
+    replaced = True
+    while replaced:
+        replaced = False
+        for k, v in {"ne": "s", "nw": "s", "se": "n", "sw": "n"}.items():
+            try:
+                a = t.index(k)
+                b = t.index(v)
+                t[a] = v + k[1]
+                del t[b]
+                replaced = 1
+            except:
+                pass
+    replaced = True
+    while replaced:
+        replaced = False
+        for k, v in {"nw": "ne", "sw": "se"}.items():
+            try:
+                a = t.index(k)
+                b = t.index(v)
+                t[a] = k[0]
+                del t[b]
+                replaced = 1
+            except:
+                pass
+    replaced = True
+    while replaced:
+        replaced = False
+        for k, v in {"n": "s"}.items():
+            try:
+                a = t.index(k)
+                b = t.index(v)
+                del t[max(a, b)]
+                del t[min(a, b)]
+                replaced = 1
+            except:
+                pass
+
+
+def easy():
+    reduce()
+    print(len(t))
+
+
+def hard():
+    global t
+    t_ = read()
+    t = []
+    m = 0
+    for w in t_:
+        t += [w]
+        reduce()
+        m = max(m, len(t))
+    print(m)
+
+
+teststr = ""  # """ne,ne,sw,sw"""
+DIR = pathlib.Path(__file__).parent.absolute()
+inf = float("inf")
+t = read()
+if __name__ == "__main__":
+    easy()
+    hard()
