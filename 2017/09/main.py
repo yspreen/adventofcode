a84731d0a75f0927d739db121340bb76c3ab3b50
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-03-14 21:22:16 +0100

    Add day 9.

---
+++ 2017/09/main.py
@@ -0,0 +1,74 @@
+import numpy as np
+import re
+import pathlib
+import json
+from functools import reduce
+from string import ascii_lowercase
+from math import prod, gcd, sqrt
+from itertools import permutations, product
+
+
+def lmap(*a):
+    return list(map(*a))
+
+
+class Group:
+    items = []
+    garbage = [0]
+
+    def __init__(self, s, start=0, score=1):
+        self.items.append(self)
+        i = start + 1
+        self.children = []
+        self.score = score
+        garbage = False
+        while i < len(s):
+            if s[i] == "!" and garbage:
+                i += 1
+            elif s[i] == ">" and garbage:
+                garbage = False
+            elif s[i] == "<" and not garbage:
+                garbage = True
+            elif s[i] == "{" and not garbage:
+                child = Group(s, i, score + 1)
+                i = child.end
+                self.children.append(child)
+            elif s[i] == "}" and not garbage:
+                break
+            elif garbage:
+                self.garbage[0] += 1
+
+            i += 1
+        self.end = i
+
+
+"""
+
+
+{{abcdef}}
+0123456790
+
+"""
+
+
+def read():
+    with open(DIR / "input") as f:
+        s = f.read() if teststr == "" else teststr
+    return Group(s.splitlines()[0])
+
+
+def easy():
+    print(sum([i.score for i in Group.items]))
+
+
+def hard():
+    print(Group.garbage[0])
+
+
+teststr = ""  #'{<{o"i!a,<{i<a>}'  # """{{{},{},{{}}}}"""
+DIR = pathlib.Path(__file__).parent.absolute()
+inf = float("inf")
+t = read()
+if __name__ == "__main__":
+    easy()
+    hard()
