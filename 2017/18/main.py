6c627482f4e3c76b1b121f3f0947f51d8c06798e
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-03-20 20:14:39 +0100

    Add part two.

--- 2017/18/main.py
+++ 2017/18/main.py
@@ -25,38 +25,70 @@ def read():
     return lmap(lambda r: lmap(sint, r.split(" ")), s.splitlines())
 
 
-def easy():
-    R = {k: 0 for k in ascii_lowercase[:16]}
-    sound = i = 0
-    while True:
-        r = t[i]
+class VM:
+    def __init__(self, id=0):
+        self.R = {k: 0 for k in ascii_lowercase[:16]}
+        self.sound = self.i = 0
+        self.R["p"] = id
+        self.buf = []
+        self.locked = False
+
+    def step(self, rcv=None, part_one=False):
+        self.locked = False
+        if rcv is not None:
+            self.buf.append(rcv)
+        r = t[self.i]
         o = 1
         a = r[1]
+        v = None
         if isinstance(a, str) and r[0] == "jgz":
-            a = R[a]
+            a = self.R[a]
         if len(r) > 2:
             b = r[2]
             if isinstance(b, str):
-                b = R[b]
+                b = self.R[b]
         if r[0] == "snd":
-            sound = R[a]
+            if part_one:
+                self.sound = self.R[a]
+            else:
+                self.sound += 1
+                v = self.R[a]
         if r[0] == "set":
-            R[a] = b
+            self.R[a] = b
         if r[0] == "add":
-            R[a] += b
+            self.R[a] += b
         if r[0] == "mul":
-            R[a] *= b
+            self.R[a] *= b
         if r[0] == "mod":
-            R[a] %= b
-        if r[0] == "rcv" and a != 0:
-            return print(sound)
+            self.R[a] %= b
+        if r[0] == "rcv":
+            if part_one and a != 0:
+                print(self.sound)
+                return 1
+            if not part_one:
+                if not self.buf:
+                    self.locked = True
+                    return
+                self.R[a] = self.buf.pop(0)
         if r[0] == "jgz" and a > 0:
             o = b
-        i += o
+        self.i += o
+        return v
+
+
+def easy():
+    v = VM()
+    while not v.step(part_one=1):
+        continue
 
 
 def hard():
-    return
+    a, b, i = VM(0), VM(1), None
+
+    while not a.locked or not b.locked:
+        i = a.step(i)
+        i = b.step(i)
+    print(b.sound)
 
 
 teststr = """"""
