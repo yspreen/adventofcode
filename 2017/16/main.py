4ec31dd11621e2b67bd76400379b9b8f510f1ba2
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-03-20 19:01:54 +0100

    Add day 16.

--- 2017/16/main.py
+++ 2017/16/main.py
@@ -39,28 +39,29 @@ def op_p(T, a, b):
     return op_x(T, T.index(a), T.index(b))
 
 
-def easy():
-    global T
+def loop(T=None):
+    T = list(ascii_lowercase[:16]) if T is None else T
     for r in t:
         T = {"s": op_s, "x": op_x, "p": op_p}[r[0]](T, *r[1:])
-    print("".join(T))
+    return T
+
+
+def easy():
+    print("".join(loop()))
 
 
 def hard():
-    global T
-    a = lmap(lambda i: ascii_lowercase.index(i), T)
-    print(a)
-    for _ in range(1):
-        b = list(range(16))
-        for q in range(10):
-            b = lmap(lambda i: b[a[i]], range(16))
-            if q == 0:
-                print("x", b)
-        a = b
-    print("".join(lmap(lambda i: ascii_lowercase[i], a)))
-    T = list(ascii_lowercase[:16])
-    for r in t * 10:
-        T = {"s": op_s, "x": op_x, "p": op_p}[r[0]](T, *r[1:])
+    T, s, n = None, set(["".join(ascii_lowercase[:16])]), 0
+    while True:
+        n += 1
+        T = loop(T)
+        c = "".join(T)
+        if c in s:
+            break
+        s.add(c)
+    T = None
+    for _ in range(1000000000 % n):
+        T = loop(T)
     print("".join(T))
 
 
@@ -68,7 +69,6 @@ teststr = """"""
 DIR = pathlib.Path(__file__).parent.absolute()
 inf = float("inf")
 t = read()
-T = list(ascii_lowercase[:16])
 if __name__ == "__main__":
     easy()
     hard()
