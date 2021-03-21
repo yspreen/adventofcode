75ab02980afd02cbb19daeb9905818b0d0850765
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-03-21 22:34:43 +0100

    Add next script.

---
+++ next.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+main() {
+    ( ! ( [ -f ./.meta ] || [ -f ../.meta ] ) ) && return
+    [ "$(cat .meta | head -1)" = 'dir=>DAY' ] && cd ..
+    n="$(ls -1 | grep -E '^\d{2}$' | sort | tail -1)"
+    n=$((n+1))
+    n="$(printf "%02d" $n)"
+    aoc -d $n
+    cd $n
+    cp ../main.py .
+    code main.py $n.md
+}
+main
