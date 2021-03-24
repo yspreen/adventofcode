fd97e2f69773122c4691d1f4e64ea4a72e73f521
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-03-25 00:29:34 +0100

    Add 24 part 1.

---
+++ 2017/24/main.py
@@ -0,0 +1,52 @@
+import numpy as np
+import re
+import pathlib
+import json
+from functools import reduce
+from string import ascii_lowercase
+from math import prod, gcd, sqrt
+from itertools import permutations, product
+
+
+def lmap(*a):
+    return list(map(*a))
+
+
+def read():
+    with open(DIR / "input") as f:
+        s = (f.read() if teststr == "" else teststr).splitlines()
+    return lmap(lambda r: lmap(int, r.split("/")), s)
+
+
+def resolve(a, seen, value=0):
+    s = []
+    for b in T.get(a, []):
+        if (a, b) in seen:
+            continue
+        s.extend(resolve(b, seen | {(a, b), (b, a)}, value + a + b))
+    if not s and value > 0:
+        s = [value]
+    return s
+
+
+def easy():
+    for a, b in t:
+        T[a] = T.get(a, []) + [b]
+        T[b] = T.get(b, []) + [a]
+
+    s = resolve(0, set())
+    print(max(s))
+
+
+def hard():
+    return
+
+
+teststr = """"""
+DIR = pathlib.Path(__file__).parent.absolute()
+inf = float("inf")
+t = read()
+T = {}
+if __name__ == "__main__":
+    easy()
+    hard()
