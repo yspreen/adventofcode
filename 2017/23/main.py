b77e5381e3518ca1d148ee0f99e89fc4d793f6b4
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-03-24 23:39:46 +0100

    Add part two.

--- 2017/23/main.py
+++ 2017/23/main.py
@@ -5,7 +5,11 @@ import json
 from functools import reduce
 from string import ascii_lowercase
 from math import prod, gcd, sqrt
-from itertools import permutations, product
+from itertools import permutations, product, count, islice
+
+
+def is_prime(n):
+    return n > 1 and all(n % i for i in islice(count(2), int(sqrt(n) - 1)))
 
 
 def lmap(*a):
@@ -45,10 +49,9 @@ class VM:
         # v = None
         if isinstance(a, str) and r[0] == "jnz":
             a = self.R[a]
-        if len(r) > 2:
-            b = r[2]
-            if isinstance(b, str):
-                b = self.R[b]
+        b = r[2]
+        if isinstance(b, str):
+            b = self.R[b]
         # if r[0] == "snd":
         #     if part_one:
         #         self.sound = self.R[a]
@@ -57,15 +60,11 @@ class VM:
         #         v = self.R[a]
         if r[0] == "set":
             self.R[a] = b
-        if r[0] == "add":
-            self.R[a] += b
         if r[0] == "sub":
             self.R[a] -= b
         if r[0] == "mul":
             self.mul_count += 1
             self.R[a] *= b
-        if r[0] == "mod":
-            self.R[a] %= b
         # if r[0] == "rcv":
         #     if part_one and a != 0:
         #         print(self.sound)
@@ -89,8 +88,50 @@ def easy():
     print(v.mul_count)
 
 
+def print_op(o, i):
+    code, A, B = o
+    sa = ("$%s" % A.upper()) if isinstance(A, str) else str(A)
+    sb = ("$%s" % B.upper()) if isinstance(B, str) else str(B)
+
+    jumps = []
+    if code == "sub":
+        s = "%s -= %s" % (sa, sb)
+    if code == "mul":
+        s = "%s *= %s" % (sa, sb)
+    if code == "set":
+        s = "%s = %s" % (sa, sb)
+    if code == "jnz":
+        s = "GOTO %s" % (B + i)
+        if B + i >= N:
+            s = "End."
+        else:
+            jumps = [(B + i, i)]
+        if sa != "1":
+            s += " IF %s != 0" % sa
+    s = s.replace("-= -", "+= ")
+    return jumps, "%3d:  %s" % (i, s)
+
+
+def reverse_code():
+    S, J = [], []
+    for i, o in enumerate(t):
+        # S.append(print_op(o, i))
+        j, s = print_op(o, i)
+        J.extend(j)
+        S.append(["   ", s])
+    for j, o in J:
+        S[j][0] = "%2d>" % o
+    for pre, s in S:
+        print(pre, s)
+
+
 def hard():
-    return
+    # reverse_code()
+    c = 0
+    for i in range(108400, 125400 + 1)[::17]:
+        if not is_prime(i):
+            c += 1
+    print(c)
 
 
 teststr = """"""
@@ -99,5 +140,5 @@ inf = float("inf")
 t = read()
 N = len(t)
 if __name__ == "__main__":
-    easy()
+    # easy()
     hard()
