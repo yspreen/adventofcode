55baa9c9961d61d0f0076e3d1ebd51ae369b76fd
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-09-04 16:41:17 +0200

    Clean up code.

--- 2016/16/main.py
+++ 2016/16/main.py
@@ -1,11 +1,5 @@
 import numpy as np
-import re
 import pathlib
-import json
-from functools import reduce
-from string import ascii_lowercase
-from math import prod, gcd, sqrt
-from itertools import permutations, product
 
 
 def lmap(*a):
@@ -56,7 +50,6 @@ def hard():
 
 teststr = """"""
 DIR = pathlib.Path(__file__).parent.absolute()
-inf = float("inf")
 t = read()
 if __name__ == "__main__":
     easy()
