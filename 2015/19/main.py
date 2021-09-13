d6d1b53d1d2ffec98fd15eb37461cbad22c89a14
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-09-13 17:31:31 +0200

    Remove unused imports.

--- 2015/19/main.py
+++ 2015/19/main.py
@@ -1,18 +1,4 @@
-import numpy as np
-import re
 import pathlib
-import json
-from functools import reduce
-from string import ascii_lowercase
-from math import cos, prod, gcd, sqrt
-from itertools import permutations, product
-from llist import dllist as llist
-from copy import deepcopy
-from hashlib import md5, sha256
-
-
-def lmap(*a):
-    return list(map(*a))
 
 
 def read():
@@ -70,7 +56,6 @@ def hard():
 
 teststr = """"""
 DIR = pathlib.Path(__file__).parent.absolute()
-inf = float("inf")
 repl, t = read()
 rev = {}
 for k, v in repl.items():
