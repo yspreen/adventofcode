3267e2e5c44fd7ce1edfd761a813a3ea16700ddc
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-09-14 20:50:41 +0200

    Add day 3.

---
+++ 2015/03/main.py
@@ -0,0 +1,39 @@
+import pathlib
+
+
+def lmap(*a):
+    return list(map(*a))
+
+
+move = {"v": (0, 1), "^": (0, -1), "<": (-1, 0), ">": (1, 0)}
+
+
+def read():
+    with open(DIR / "input") as f:
+        s = (f.read() if teststr == "" else teststr).splitlines()[0]
+    return lmap(lambda r: move[r], s)
+
+
+def easy():
+    p, visited = (0, 0), {(0, 0)}
+    for m in t:
+        p = (p[0] + m[0], p[1] + m[1])
+        visited.add(p)
+    print(len(visited))
+
+
+def hard():
+    p, visited, i = [(0, 0), (0, 0)], [{(0, 0)}, {(0, 0)}], 0
+    for m in t:
+        p[i] = (p[i][0] + m[0], p[i][1] + m[1])
+        visited[i].add(p[i])
+        i = 1 - i
+    print(len(visited[0] | visited[1]))
+
+
+teststr = """"""
+DIR = pathlib.Path(__file__).parent.absolute()
+t = read()
+if __name__ == "__main__":
+    easy()
+    hard()
