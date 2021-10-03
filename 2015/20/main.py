533933e6168b58036350a791aade14ff4fa1dffc
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-10-03 21:02:48 +0200

    Add day 20.
    Cheating a bit with the limits.

--- 2015/20/main.py
+++ 2015/20/main.py
@@ -1,14 +1,5 @@
-import numpy as np
-import re
 import pathlib
-import json
-from functools import reduce
-from string import ascii_lowercase
-from math import prod, gcd, sqrt
-from itertools import permutations, product
-from llist import dllist as llist
-from copy import deepcopy
-from hashlib import md5, sha256
+from math import sqrt
 
 
 def read():
@@ -16,53 +7,43 @@ def read():
         return (f.read() if teststr == "" else teststr).splitlines()[0]
 
 
-import itertools
-
-
-def erat2():
-    D = {}
-    yield 2
-    for q in itertools.islice(itertools.count(3), 0, None, 2):
-        p = D.pop(q, None)
-        if p is None:
-            D[q * q] = q
-            yield q
-        else:
-            x = p + q
-            while x in D or not (x & 1):
-                x += p
-            D[x] = p
-
-
 def presents(x):
     sq = int(sqrt(x)) + 1
-    values = [1, x]
-    for prime in erat2():
-        if prime > sq:
-            break
-        val = prime
-        while True:
-            if val >= x or x % val > 0:
-                break
-            values.append(val)
-            val += prime
+    values = {1, x}
+    for num in range(2, sq):
+        if x % num == 0:
+            values.add(num)
+            values.add(x // num)
     return sum(values) * 10
 
 
+def presents2(x):
+    sq = int(sqrt(x)) + 1
+    values = {1, x}
+    for num in range(2, sq):
+        if x % num == 0:
+            s = x // num
+            if s <= 50:
+                values.add(num)
+            if num <= 50:
+                values.add(s)
+    return sum(values) * 11
+
+
 def easy():
-    for i in range(int(9e9)):
+    for i in range(int(8e5), t)[::8]:
         if presents(i) >= t:
             return print(i)
 
 
 def hard():
-    return
+    for i in range(int(8e5), t)[::8]:
+        if presents2(i) >= t:
+            return print(i)
 
 
 teststr = """"""
 DIR = pathlib.Path(__file__).parent.absolute()
-lmap = lambda *a: list(map(*a))
-inf = float("inf")
 t = int(read())
 if __name__ == "__main__":
     easy()
