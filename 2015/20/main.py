206c3e5cdba5aad3fee4f7932f937cacc543e2d3
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-10-05 17:50:30 +0200

    Change to sieve alg.

--- 2015/20/main.py
+++ 2015/20/main.py
@@ -7,34 +7,20 @@ def read():
         return (f.read() if teststr == "" else teststr).splitlines()[0]
 
 
-def presents(x, limit=9e9, mult=10):
-    sq = int(sqrt(x)) + 1
-    values = {1, x}
-    for num in range(2, sq):
-        if x % num == 0:
-            s = x // num
-            if s <= limit:
-                values.add(num)
-            if num <= limit:
-                values.add(s)
-    return sum(values) * mult
-
-
-def easy():
-    for i in range(int(8e5), t)[::8]:
-        if presents(i) >= t:
-            return print(i)
-
-
-def hard():
-    for i in range(int(8e5), t)[::8]:
-        if presents(i, 50, 11) >= t:
-            return print(i)
+def presents(max_fact=9e9, mult=10, limit=1e6):
+    max_fact, limit, houses = int(max_fact), int(limit), [0] * int(limit)
+    for i in range(limit):
+        for k, j in enumerate(range(limit)[:: (i + 1)][1:]):
+            if k == max_fact or j >= limit:
+                break
+            houses[j] += i * mult
+            if houses[j] >= t:
+                return print(j)
 
 
 teststr = """"""
 DIR = pathlib.Path(__file__).parent.absolute()
 t = int(read())
 if __name__ == "__main__":
-    easy()
-    hard()
+    presents()
+    presents(50, 11)
