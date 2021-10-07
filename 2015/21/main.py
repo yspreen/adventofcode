e17011fbaff5471517c55a9176abe76c2d2a002e
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-10-07 21:58:20 +0200

    Shorten code.

--- 2015/21/main.py
+++ 2015/21/main.py
@@ -17,22 +17,6 @@ def wins(_, dmg, amr):
     return enemy_t >= turns
 
 
-def easy():
-    m = 9e9
-    for w, a, r in product(W, A, R):
-        if wins(*(w + a + r)):
-            m = min(m, (w + a + r)[0])
-    print(m)
-
-
-def hard():
-    m = 0
-    for w, a, r in product(W, A, R):
-        if not wins(*(w + a + r)):
-            m = max(m, (w + a + r)[0])
-    print(m)
-
-
 W = np.array([[8, 4, 0], [10, 5, 0], [25, 6, 0], [40, 7, 0], [74, 8, 0]])
 A = np.array([[13, 0, 1], [31, 0, 2], [53, 0, 3], [75, 0, 4], [102, 0, 5]])
 R = np.array([[25, 1, 0], [50, 2, 0], [100, 3, 0], [20, 0, 1], [40, 0, 2], [80, 0, 3]])
@@ -46,5 +30,6 @@ DIR = pathlib.Path(__file__).parent.absolute()
 lmap = lambda *a: list(map(*a))
 t = read()
 if __name__ == "__main__":
-    easy()
-    hard()
+    win = [(wins(*(w + a + r)), (w + a + r)[0]) for w, a, r in product(W, A, R)]
+    print(min([cost for wins, cost in win if wins]))
+    print(max([cost for wins, cost in win if not wins]))
