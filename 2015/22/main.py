23f55a9c22f088a9034ce0a1b6c752deabf30bf4
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-10-12 18:03:01 +0200

    Add part two and clean up.

--- 2015/22/main.py
+++ 2015/22/main.py
@@ -1,14 +1,4 @@
-import numpy as np
-import re
 import pathlib
-import json
-from functools import reduce
-from string import ascii_lowercase
-from math import prod, gcd, sqrt
-from itertools import permutations, product
-from llist import dllist as llist
-from copy import deepcopy
-from hashlib import md5, sha256
 
 
 class State:
@@ -23,18 +13,7 @@ class State:
         self.amount_armor = 7
         self.time_poi = 0
         self.amount_poi = 3
-        self.dead = False
-        self.won = False
         self.mana_used = 0
-
-    def print(self, nl=1):
-        print(
-            "Player has %d hit points, %d armor, %d mana"
-            % (self.hp1, self.armor, self.mana)
-        )
-        print("Boss has %d hit points" % (self.hp2))
-        if nl:
-            print()
 
     def clone(self):
         s = State()
@@ -48,16 +27,10 @@ class State:
         s.amount_armor = self.amount_armor
         s.time_poi = self.time_poi
         s.amount_poi = self.amount_poi
-        s.dead = self.dead
-        s.won = self.won
         s.mana_used = self.mana_used
         return s
 
     def step(self):
-        if self.dead:
-            return False
-        if self.won:
-            return True
         if self.time_mana > 0:
             self.time_mana -= 1
             self.mana += self.amount_mana
@@ -67,21 +40,17 @@ class State:
             self.time_poi -= 1
             self.hp2 -= self.amount_poi
             if self.hp2 <= 0:
-                self.won = True
                 return True
 
     @property
     def armor(self):
         return self.amount_armor if self.time_armor > 0 else 0
 
-    def action(self, type, printout=0):
-        if printout:
-            print("-- Player turn --")
-            self.print(nl=0)
-            print(
-                "Player casts %s\n"
-                % (["Magic Missile", "Drain", "Shield", "Poison", "Recharge"][type])
-            )
+    def action(self, type, hard=0):
+        if hard:
+            self.hp1 -= 1
+            if self.hp1 <= 0:
+                return False
         s = self.step()
         if s is not None:
             return s
@@ -107,21 +76,14 @@ class State:
             self.mana_used += 229
             self.time_mana = 5
         if self.mana < 0:
-            self.dead = True
             return False
         if self.hp2 <= 0:
-            self.won = True
             return True
-        if printout:
-            print("-- Boss turn --")
-            self.print(nl=0)
-            print("Boss does %s damage\n" % max(self.dmg2 - self.armor, 1))
         s = self.step()
         if s is not None:
             return s
         self.hp1 -= max(self.dmg2 - self.armor, 1)
         if self.hp1 <= 0:
-            self.dead = True
             return False
 
 
@@ -131,27 +93,14 @@ def read():
     return lmap(lambda r: int(r.split(" ")[-1]), s)
 
 
-def easy():
-    # s = State(8, 10, 13, 250)
-    # s.action(3, printout=1)
-    # s.action(1, printout=1)
-    # print(s.won, s.dead, "\n")
-    # s = State(8, 10, 14, 250)
-    # s.action(4, printout=1)
-    # s.action(2, printout=1)
-    # s.action(1, printout=1)
-    # s.action(3, printout=1)
-    # s.action(0, printout=1)
-    # print(s.won, s.dead)
-
-    # return
+def run(hard=0):
     pos, m = [State()], 9e9
     while pos:
         new = []
         for original in pos:
             for i in range(5):
                 p = original.clone()
-                won = p.action(i)
+                won = p.action(i, hard=hard)
                 if won is None:
                     new.append(p)
                 elif won:
@@ -161,14 +110,17 @@ def easy():
             return print(m)
 
 
+def easy():
+    run()
+
+
 def hard():
-    return
+    run(hard=1)
 
 
 teststr = """"""
 DIR = pathlib.Path(__file__).parent.absolute()
 lmap = lambda *a: list(map(*a))
-inf = float("inf")
 t = read()
 if __name__ == "__main__":
     easy()
