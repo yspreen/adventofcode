56ac8911b5c4d6eb9043fd8c294a9f90a9b2535b
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-12-06 12:56:48 +0100

    Clean up imports.

--- 2021/06/main.py
+++ 2021/06/main.py
@@ -1,24 +1,15 @@
 import numpy as np
-import re
 import pathlib
-import json
-from functools import reduce
-from string import ascii_lowercase
-from math import prod, gcd, sqrt
-from itertools import permutations, product
-from llist import dllist as llist
-from copy import deepcopy
-from hashlib import md5, sha256
 
 
 def read():
     with open(DIR / "input") as f:
-        s = (f.read() if teststr == "" else teststr).splitlines()[0]
-    return np.array([s.count(str(i)) for i in range(9)], dtype=np.uint64)
+        s = f.read() if teststr == "" else teststr
+    return [s.count(str(i)) for i in range(9)]
 
 
 def run(days):
-    a = np.array(list(t), dtype=np.uint64)
+    a = np.array(t, dtype=np.uint64)
     for _ in range(days):
         a = np.roll(a, -1)
         a[6] += a[8]
@@ -27,8 +18,6 @@ def run(days):
 
 teststr = """"""
 DIR = pathlib.Path(__file__).parent.absolute()
-lmap = lambda *a: list(map(*a))
-inf = float("inf")
 t = read()
 if __name__ == "__main__":
     print(run(80))
