9a26b25ddce622b756ce13a06848e8ec82b63606
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2021-12-23 01:03:06 +0100

    Add part two of day 22.

--- 2021/22/main.py
+++ 2021/22/main.py
@@ -1,14 +1,6 @@
 import numpy as np
 import re
 import pathlib
-import json
-from functools import reduce
-from string import ascii_lowercase
-from math import prod, gcd, sqrt
-from itertools import permutations, product
-from llist import dllist as llist
-from copy import deepcopy
-from hashlib import md5, sha256
 
 
 def read():
@@ -47,37 +39,59 @@ def easy():
     print(A.sum())
 
 
+class Cube:
+    def __init__(self, x1, x2, y1, y2, z1, z2, operation=1) -> None:
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        self.z1 = z1
+        self.z2 = z2
+        self.operation = operation
+        self.subtract = []
+        self.volume = (x2 - x1 + 1) * (y2 - y1 + 1) * (z2 - z1 + 1) * operation
+
+    def intersect(self, other):
+        if not self.intersects(other):
+            return
+        x1 = max(self.x1, other.x1)
+        x2 = min(self.x2, other.x2)
+        y1 = max(self.y1, other.y1)
+        y2 = min(self.y2, other.y2)
+        z1 = max(self.z1, other.z1)
+        z2 = min(self.z2, other.z2)
+
+        new_sub = Cube(x1, x2, y1, y2, z1, z2, -self.operation)
+        for sub in self.subtract:
+            sub.intersect(new_sub)
+        self.subtract.append(new_sub)
+
+    def total_volume(self):
+        return self.volume + sum([s.total_volume() for s in self.subtract])
+
+    def intersects(self, other):
+        if self.x1 > other.x2 or other.x1 > self.x2:
+            return False
+        if self.y1 > other.y2 or other.y1 > self.y2:
+            return False
+        if self.z1 > other.z2 or other.z1 > self.z2:
+            return False
+        return True
+
+
 def hard():
-    return
+    for cube, on_off in [(Cube(*c[1:]), c[0]) for c in t]:
+        for other in cubes:
+            other.intersect(cube)
+        if on_off:
+            cubes.append(cube)
+    print(sum([c.total_volume() for c in cubes]))
 
 
-teststr = """on x=-20..26,y=-36..17,z=-47..7
-on x=-20..33,y=-21..23,z=-26..28
-on x=-22..28,y=-29..23,z=-38..16
-on x=-46..7,y=-6..46,z=-50..-1
-on x=-49..1,y=-3..46,z=-24..28
-on x=2..47,y=-22..22,z=-23..27
-on x=-27..23,y=-28..26,z=-21..29
-on x=-39..5,y=-6..47,z=-3..44
-on x=-30..21,y=-8..43,z=-13..34
-on x=-22..26,y=-27..20,z=-29..19
-off x=-48..-32,y=26..41,z=-47..-37
-on x=-12..35,y=6..50,z=-50..-2
-off x=-48..-32,y=-32..-16,z=-15..-5
-on x=-18..26,y=-33..15,z=-7..46
-off x=-40..-22,y=-38..-28,z=23..41
-on x=-16..35,y=-41..10,z=-47..6
-off x=-32..-23,y=11..30,z=-14..3
-on x=-49..-5,y=-3..45,z=-29..18
-off x=18..30,y=-20..-8,z=-3..13
-on x=-41..9,y=-7..43,z=-33..15
-on x=-54112..-39298,y=-85059..-49293,z=-27449..7877
-on x=967..23432,y=45373..81175,z=27513..53682"""
 teststr = ""
 DIR = pathlib.Path(__file__).parent.absolute()
 lmap = lambda *a: list(map(*a))
-inf = float("inf")
-t = read()
+t, cubes = read(), []
 if __name__ == "__main__":
     easy()
     hard()
