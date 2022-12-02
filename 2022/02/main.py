bf1abef5489b4b1a614b31de1e034ebd433ac63f
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2022-12-02 16:59:41 -0500

    Clean up day 2.

--- 2022/02/main.py
+++ 2022/02/main.py
@@ -17,58 +17,49 @@ def read():
     return lmap(lambda r: lmap(str, r.split(" ")), s)
 
 
+move_to_score = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3,
+}
+turn_to_win = {
+    "AX": 3,
+    "AY": 6,
+    "AZ": 0,
+    "BX": 0,
+    "BY": 3,
+    "BZ": 6,
+    "CX": 6,
+    "CY": 0,
+    "CZ": 3,
+}
+outcome_to_turn = {
+    "AX": "Z",
+    "AY": "X",
+    "AZ": "Y",
+    "BX": "X",
+    "BY": "Y",
+    "BZ": "Z",
+    "CX": "Y",
+    "CY": "Z",
+    "CZ": "X",
+}
+
+
 def easy():
     s = 0
     for theirs, mine in t:
-        s += {
-            "X": 1,
-            "Y": 2,
-            "Z": 3,
-        }[mine]
-        s += {
-            "AX": 3,
-            "AY": 6,
-            "AZ": 0,
-            "BX": 0,
-            "BY": 3,
-            "BZ": 6,
-            "CX": 6,
-            "CY": 0,
-            "CZ": 3,
-        }[theirs + mine]
+        s += move_to_score[mine]
+        s += turn_to_win[theirs + mine]
     print(s)
 
 
 def hard():
     s = 0
     for theirs, outcome in t:
-        mine = {
-            "AX": "Z",
-            "AY": "X",
-            "AZ": "Y",
-            "BX": "X",
-            "BY": "Y",
-            "BZ": "Z",
-            "CX": "Y",
-            "CY": "Z",
-            "CZ": "X",
-        }[theirs + outcome]
-        s += {
-            "X": 1,
-            "Y": 2,
-            "Z": 3,
-        }[mine]
-        s += {
-            "AX": 3,
-            "AY": 6,
-            "AZ": 0,
-            "BX": 0,
-            "BY": 3,
-            "BZ": 6,
-            "CX": 6,
-            "CY": 0,
-            "CZ": 3,
-        }[theirs + mine]
+        mine = outcome_to_turn[theirs + outcome]
+        s += move_to_score[mine]
+        s += turn_to_win[theirs + mine]
     print(s)
 
 
