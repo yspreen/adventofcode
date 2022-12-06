10e7eee0d1231e5adb2025800f6006827b66f793
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2022-12-06 13:40:29 -0500

    Add day 6.

---
+++ 2022/06/main.py
@@ -0,0 +1,44 @@
+import numpy as np
+import re
+import pathlib
+import json
+from functools import reduce
+from string import ascii_lowercase
+from math import prod, gcd, sqrt
+from itertools import permutations, product
+from llist import dllist as llist
+from copy import deepcopy
+from hashlib import md5, sha256
+
+
+def read():
+    with open(DIR / "input") as f:
+        s = (f.read() if teststr == "" else teststr).splitlines()
+    return s[0]
+
+
+def easy():
+    run(4)
+
+
+def hard():
+    run(14)
+
+
+def run(num):
+    b, i = t[:num], num
+    while len(set(b)) < num:
+        b += t[i]
+        b = b[1:]
+        i += 1
+    print(i)
+
+
+teststr = """mjqjpqmgbljsphdztnvjfqwrcgsmlb"""
+DIR = pathlib.Path(__file__).parent.absolute()
+lmap = lambda *a: list(map(*a))
+inf = float("inf")
+t = read()
+if __name__ == "__main__":
+    easy()
+    hard()
