a37c745d5904fe740ffb42b2cc7b78bac890fb7a
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2022-12-21 00:31:33 -0500

    After stream.

---
+++ 2022/21/main.py
@@ -0,0 +1,113 @@
+import numpy as np
+import re
+import pathlib
+import json
+from functools import reduce
+from string import ascii_lowercase
+from math import prod, gcd, sqrt
+from itertools import permutations, product
+from llist import dllist as llist
+from copy import deepcopy
+from hashlib import md5, sha256
+from sympy import symbols, solve
+
+
+def read():
+    with open(DIR / "input") as f:
+        s = (f.read() if teststr == "" else teststr).splitlines()
+    return lmap(lambda r: r.replace(": ", " = "), s)
+
+
+mv = [
+    (-1, 0),  # U
+    (1, 0),  # D
+    (0, -1),  # L
+    (0, 1),  # R
+]
+
+
+def subs(vars, s):
+    for key, value in vars.items():
+        s = s.replace(key, str(value))
+    return s
+
+
+def easy():
+    solved = set([])
+    root = None
+    vars = {}
+    while "root" not in solved:
+        for row in t:
+            start = row.split(" = ")[0]
+            end = row.split(" = ")[1]
+            if start in solved:
+                continue
+            if re.sub(r"[0-9\s]+", "", end) == "":
+                vars[start] = eval(subs(vars, end))
+                solved.add(start)
+                continue
+            parts = end.split(" ")
+            var_a = parts[0]
+            var_b = parts[-1]
+            if var_a not in solved or var_b not in solved:
+                continue
+            vars[start] = eval(subs(vars, end))
+            solved.add(start)
+
+    print(int(vars["root"]))
+
+
+def hard():
+    from sympy.core.sympify import sympify
+    from sympy.parsing.sympy_parser import parse_expr
+    from sympy import simplify
+
+    statements = {}
+    for row in t:
+        start = row.split(" = ")[0]
+        end = f"({row.split(' = ')[1]})"
+        statements[start] = end
+
+    lhs, rhs = statements["root"].replace("(", "").replace(")", "").split("+")
+
+    didrepl = True
+    while didrepl:
+        didrepl = False
+        for key, value in statements.items():
+            if key == "humn":
+                value = " x "
+            if key in lhs:
+                didrepl = True
+
+                lhs = lhs.replace(key, value)
+                # print(lhs)
+                # lhs = str(simplify(sympify(lhs)))
+    didrepl = True
+    while didrepl:
+        didrepl = False
+        for key, value in statements.items():
+            if key == "humn":
+                value = " x "
+            if key in rhs:
+                didrepl = True
+
+                rhs = rhs.replace(key, value)
+                # print(rhs)
+                # rhs = str(simplify(sympify(rhs)))
+
+    lhs = simplify(sympify(lhs))
+    rhs = simplify(sympify(rhs))
+
+    sol = solve(lhs - rhs)
+
+    print(str(sol)[1:-1])
+
+
+teststr = """"""
+DIR = pathlib.Path(__file__).parent.absolute()
+lmap = lambda *a: list(map(*a))
+inf = float("inf")
+t = read()
+if __name__ == "__main__":
+    easy()
+    hard()
