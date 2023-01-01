0ad3373298d49d0ce5d7f966022c18c508282393
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2023-01-01 17:41:29 -0500

    New optimizations.
    Never produce more resource than we will ever need. See how many geodes can be achieved by one timeline max.

--- 2022/19/main.py
+++ 2022/19/main.py
@@ -41,6 +41,10 @@ class Blueprint:
         self.geo_cost_ore = geo_cost_ore
         self.geo_cost_silver = geo_cost_silver
 
+        self.max_silver = geo_cost_silver * N
+        self.max_ore = geo_cost_ore * N + self.max_silver * silver_cost_ore
+        self.max_clay = self.max_silver * silver_cost_ore
+
     def can_build(self, resources, robot):
         if robot == "O":
             return resources.ore >= self.ore_cost_ore
@@ -122,16 +126,30 @@ class Timeline:
         self.resources = resources
         self.robots = robots
 
-    def futures(self):
-        next_possible = []
-        for next_robot in ["O", "C", "S", "G"]:
+    def futures(self, max_geodes, time_left):
+        if time_left + self.resources.geo < max_geodes:
+            return [], False
+        next_possible = set()
+        did_build_geode = False
+        if self.blueprint.can_build(self.resources, "G"):
+            next_possible.add("G")
+            did_build_geode = True
+        for next_robot in {"O", "C", "S"}:
             if self.blueprint.can_build(self.resources, next_robot):
-                next_possible.append(next_robot)
+                next_possible.add(next_robot)
         self.resources = self.resources.add(self.robots)
         futures = [self]
         for next_robot in next_possible:
+            if next_robot == "O" and self.resources.ore >= self.blueprint.max_ore:
+                continue
+            if next_robot == "C" and self.resources.clay >= self.blueprint.max_clay:
+                continue
+            if next_robot == "S" and self.resources.silver >= self.blueprint.max_silver:
+                continue
+            if next_robot != "G" and time_left + self.resources.geo - 1 < max_geodes:
+                continue
             futures.append(self.build(next_robot))
-        return futures
+        return futures, did_build_geode
 
     def build(self, robot):
         return Timeline(
@@ -145,13 +163,18 @@ class Timeline:
 
 
 def run_blueprint(blueprint):
-    m = 0
+    max_geodes = 0
     timelines = [Timeline(blueprint, Resources(), Robots(ore=1))]
-    for j in range(24):
-        print(j)
+    time_left = N
+    for j in range(N):
+        time_left -= 1
+        # print(j)
         new_timelines = set()
+        prev_max_geodes = max_geodes
         for timeline in timelines:
-            futures = timeline.futures()
+            futures, did_build_geode = timeline.futures(max_geodes, time_left)
+            if did_build_geode:
+                max_geodes = prev_max_geodes + 1
             tuples = {t.to_tuple() for t in futures}
             new_timelines |= tuples
         timelines = [
@@ -161,8 +184,12 @@ def run_blueprint(blueprint):
     return max([t.resources.geo for t in timelines])
 
 
+N = 24
+
+
 def easy():
     blueprints = [Blueprint(*r) for r in t]
+    # blueprints = [blueprints[0]]
     # print(blueprints[0])
 
     from multiprocessing import Pool
