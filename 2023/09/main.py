d27a29dc0229541e8e20170f69287f469b6d52b6
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2023-12-09 00:44:13 -0500

    Add

---
+++ 2023/09/main.py
@@ -0,0 +1,124 @@
+import numpy as np
+import re
+import pathlib
+import json
+from functools import reduce
+from string import ascii_lowercase
+from math import prod, gcd, sqrt
+from itertools import permutations, product
+from llist import dllist as llist
+from copy import deepcopy
+from hashlib import md5, sha256
+
+
+def read():
+    with open(DIR / "input") as f:
+        s = (f.read() if teststr == "" else teststr).splitlines()
+    return lmap(lambda r: lmap(int, r.split(" ")), s)
+
+
+def maybeint(line):
+    try:
+        return int(line)
+    except:
+        return line
+
+
+mv = [
+    (-1, 0),  # U
+    (1, 0),  # D
+    (0, -1),  # L
+    (0, 1),  # R
+    (-1, 1),  # UR
+    (1, 1),  # DR
+    (-1, -1),  # UL
+    (1, -1),  # DL
+]
+mv_3d = [
+    (-1, 0, 0),  # U
+    (1, 0, 0),  # D
+    (0, -1, 0),  # L
+    (0, 1, 0),  # R
+    (0, 0, -1),  # B
+    (0, 0, 1),  # F
+]
+
+
+def BFS(start, can_walk, goal, cost_fn=None):
+    cost_fn = (lambda _, __: 1) if cost_fn is None else cost_fn
+
+    options = [(start, 0)]
+    visited = set([start])
+
+    while options:
+        new_o = []
+        for pos, cost in options:
+            for d in mv:
+                new_p = (pos[0] + d[0], pos[1] + d[1])
+                if new_p[0] < 0:  # lower bound check
+                    continue
+                if new_p[1] < 0:  # lower bound check
+                    continue
+                try:
+                    assert can_walk(pos, new_p)
+                except:
+                    continue  # upper bound check
+                if new_p in visited:
+                    continue
+                visited.add(new_p)
+                cost_ = cost + cost_fn(pos, new_p)
+                new_o.append((new_p, cost_))
+                if goal(new_p):
+                    return cost_
+        options = new_o
+    return None
+
+
+def all_zeros(row):
+    for i in row:
+        if i != 0:
+            return False
+    return True
+
+
+def deltas(row):
+    val = row[0]
+    diffs = []
+    for other in row[1:]:
+        diffs.append(other - val)
+        val = other
+    return diffs
+
+
+def easy():
+    sum = 0
+    for row in t:
+        M = [row]
+        while not all_zeros(M[-1]):
+            M.append(deltas(M[-1]))
+        for delta in map(lambda row: row[-1], M):
+            sum += delta
+    print(sum)
+
+
+def hard():
+    sum = 0
+    for row in t:
+        M = [row]
+        while not all_zeros(M[-1]):
+            M.append(deltas(M[-1]))
+        v = 0
+        for delta in map(lambda row: row[0], M[::-1]):
+            v = delta - v
+        sum += v
+    print(sum)
+
+
+teststr = """"""
+DIR = pathlib.Path(__file__).parent.absolute()
+lmap = lambda *a: list(map(*a))
+inf = float("inf")
+t = read()
+if __name__ == "__main__":
+    easy()
+    hard()
