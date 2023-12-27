3bf4cff50db82919f38ecd744082dd116310978f
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2023-12-27 11:41:09 -0500

    Add 13 part one.

---
+++ 2023/14/main.py
@@ -0,0 +1,125 @@
+import numpy as np
+import re
+import pathlib
+import json
+from functools import reduce
+from string import ascii_lowercase
+from math import prod, gcd, sqrt
+from itertools import permutations, product
+from llist import dllist as llist
+from copy import deepcopy
+from hashlib import md5, sha256
+
+
+def read():
+    with open(DIR / "input") as f:
+        s = (
+            (f.read() if teststr == "" else teststr)
+            .replace(".", "0")
+            .replace("#", "1")
+            .replace("O", "2")
+            .splitlines()
+        )
+    return np.array(lmap(lambda r: lmap(int, r), s))
+
+
+def maybeint(line):
+    try:
+        return int(line)
+    except:
+        return line
+
+
+mv = [
+    (-1, 0),  # U
+    (1, 0),  # D
+    (0, -1),  # L
+    (0, 1),  # R
+    (-1, 1),  # UR
+    (1, 1),  # DR
+    (-1, -1),  # UL
+    (1, -1),  # DL
+]
+mv_3d = [
+    (-1, 0, 0),  # U
+    (1, 0, 0),  # D
+    (0, -1, 0),  # L
+    (0, 1, 0),  # R
+    (0, 0, -1),  # B
+    (0, 0, 1),  # F
+]
+
+
+def BFS(start, can_walk, goal, cost_fn=None):
+    cost_fn = (lambda _, __: 1) if cost_fn is None else cost_fn
+
+    options = [(start, 0)]
+    visited = set([start])
+
+    while options:
+        new_o = []
+        for pos, cost in options:
+            for d in mv:
+                new_p = (pos[0] + d[0], pos[1] + d[1])
+                if new_p[0] < 0:  # lower bound check
+                    continue
+                if new_p[1] < 0:  # lower bound check
+                    continue
+                try:
+                    assert can_walk(pos, new_p)
+                except:
+                    continue  # upper bound check
+                if new_p in visited:
+                    continue
+                visited.add(new_p)
+                cost_ = cost + cost_fn(pos, new_p)
+                new_o.append((new_p, cost_))
+                if goal(new_p):
+                    return cost_
+        options = new_o
+    return None
+
+
+def count_up(A):
+    for x, y in zip(*np.where(A == 2)):
+        old_x = x
+        while x > 0 and A[x - 1, y] == 0:
+            x -= 1
+        A[old_x, y] = 0
+        A[x, y] = 2
+
+    answer = 0
+    for x, y in zip(*np.where(A[::-1] == 2)):
+        answer += x + 1
+
+    return answer
+
+
+def easy():
+    print(count_up(np.array(t)))  # U
+    # print(count_up(np.array(t)[::-1]))  # D
+    # print(count_up(np.array(t).T))  # L
+    # print(count_up(np.array(t).T[::-1]))  # R
+
+
+def hard():
+    return
+
+
+teststr = """O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#...."""
+DIR = pathlib.Path(__file__).parent.absolute()
+lmap = lambda *a: list(map(*a))
+inf = float("inf")
+t = read()
+if __name__ == "__main__":
+    easy()
+    hard()
