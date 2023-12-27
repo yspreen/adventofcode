ffac1a06effe40b8d730a81bed04d6e5968bb109
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2023-12-27 12:16:10 -0500

    Add day 15 part one.

---
+++ 2023/15/main.py
@@ -0,0 +1,51 @@
+import numpy as np
+import re
+import pathlib
+import json
+from functools import reduce
+from string import ascii_lowercase
+from math import prod, gcd, sqrt
+from itertools import permutations, product
+from llist import dllist as llist
+from copy import deepcopy
+from hashlib import md5, sha256
+
+
+def read():
+    with open(DIR / "input") as f:
+        s = (f.read() if teststr == "" else teststr).splitlines()[0]
+    return s.split(",")
+
+
+def maybeint(line):
+    try:
+        return int(line)
+    except:
+        return line
+
+
+def hash(string):
+    h = 0
+    for c in string:
+        h += ord(c)
+        h *= 17
+        h %= 256
+    return h
+
+
+def easy():
+    print(sum(hash(s) for s in t))
+
+
+def hard():
+    return
+
+
+teststr = """rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"""
+DIR = pathlib.Path(__file__).parent.absolute()
+lmap = lambda *a: list(map(*a))
+inf = float("inf")
+t = read()
+if __name__ == "__main__":
+    easy()
+    hard()
