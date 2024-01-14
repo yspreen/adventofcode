bc08d2f58ff096bfe7ad7d614fea058f91cbf3b6
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2024-01-14 16:42:07 -0500

    Back to sympy.

--- 2023/24/main.py
+++ 2023/24/main.py
@@ -1,6 +1,6 @@
 import pathlib
 from os import environ
-import math
+from sympy import symbols, Eq, solve
 
 
 def read():
@@ -14,23 +14,23 @@ def collides(a, b):
     bpx, bpy, bpz, bvx, bvy, bvz = b
 
     # Define new symbols for t_a and t_b
-    var("t_a t_b")
+    t_a, t_b = symbols("t_a t_b")
 
     # Redefine the equations with t_a and t_b
-    equation1_new = Eq(apx + avx * t_a == bpx + bvx * t_b)
-    equation2_new = Eq(apy + avy * t_a == bpy + bvy * t_b)
+    equation1_new = Eq(apx + avx * t_a, bpx + bvx * t_b)
+    equation2_new = Eq(apy + avy * t_a, bpy + bvy * t_b)
 
     # Solve the new set of equations
-    solution_new = solve([equation1_new, equation2_new], t_a, t_b)
+    solution_new = solve((equation1_new, equation2_new), (t_a, t_b))
 
-    # Extract the solutions for t_a and t_b
     if not solution_new:
         return False
-    t_a_solution, t_b_solution = [sol.rhs() for sol in solution_new[0]]
+    t_a_solution, t_b_solution = solution_new[t_a], solution_new[t_b]
     if t_a_solution < 0 or t_b_solution < 0:
         return False
 
     # Calculate the intersection point using either line's equation
+    # Using line a's equation for demonstration
     x = apx + avx * t_a_solution
     y = apy + avy * t_a_solution
 
@@ -62,44 +62,33 @@ def easy():
     for i in range(N):
         print(i)
         for j in range(i + 1, N):
-            if collides(t_b[i], t_b[j]):
+            if collides(t[i], t[j]):
                 c += 1
     print(c)
 
 
-def distance_pair(t, i, j, x):
-    i_px, i_py, i_pz, i_vx, i_vy, i_vz = t[i]
-    j_px, j_py, j_pz, j_vx, j_vy, j_vz = t[j]
-    return (
-        abs((i_px + x * i_vx) - (j_px + x * j_vx))
-        + abs((i_py + x * i_vy) - (j_py + x * j_vy))
-        + abs((i_pz + x * i_vz) - (j_pz + x * j_vz))
-    )
-
-
-def calc_distance(t, time):
-    d = 0
-    for i in range(len(t)):
-        for j in range(i + 1, len(t)):
-            d += distance_pair(t, i, j, time)
-    return d
-
-
 def hard():
-    digits = len(str(t[0][0]))
-    best = 0
-    for d in range(digits)[::-1]:
-        min = (0, 99e99)
-        for i in range(20):
-            i -= 10
-            i *= d
-            new_d = calc_distance(t, time=i + best)
-            if new_d < min[1]:
-                min = (i, new_d)
-        print(min)
-        best += min[0]
+    s_px, s_py, s_pz, s_vx, s_vy, s_vz = symbols("s_px s_py s_pz s_vx s_vy s_vz")
+    equations = []
+    vars = [s_px, s_py, s_pz, s_vx, s_vy, s_vz]
+    i = 0
+    for apx, apy, apz, avx, avy, avz in t:
+        t_i = symbols(f"t_{number_to_letters(i)}")
+        i += 1
+        equations.append(Eq(apx + avx * t_i, s_px + s_vx * t_i))
+        equations.append(Eq(apy + avy * t_i, s_py + s_vy * t_i))
+        equations.append(Eq(apz + avz * t_i, s_pz + s_vz * t_i))
+        vars.append(t_i)
 
-    print(best)
+    # Solve the new set of equations
+    solution_new = solve(tuple(equations), tuple(vars))
+
+    # Extract the solutions for t_b and t_b
+    if not solution_new:
+        return
+
+    for s in [s_px, s_py, s_pz, s_vx, s_vy, s_vz]:
+        print(solution_new[s])
 
 
 teststr = """19, 13, 30 @ -2,  1, -2
@@ -107,12 +96,12 @@ teststr = """19, 13, 30 @ -2,  1, -2
 20, 25, 34 @ -2, -2, -4
 12, 31, 28 @ -1, -2, -1
 20, 19, 15 @  1, -5, -3"""
-if environ.get("AOC_SOLVE", "") == "1":
+if environ.get("AOC_SOLVE", "1") == "1":
     teststr = ""
 DIR = pathlib.Path(__file__).parent.absolute()
 lmap = lambda *a: list(map(*a))
 t = read()
 N = len(t)
 if __name__ == "__main__":
-    # easy()
+    easy()
     hard()
