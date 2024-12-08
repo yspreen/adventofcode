33aa2f3bc1420cac9dee86dc1254637f99a2ee4a
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2024-12-08 17:02:00 -0500

    Update req.

--- next.sh
+++ next.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 main() {
-    ( ! ( [ -f ./.meta ] || [ -f ../.meta ] ) ) && return
+    ( ! ([ -f ./.meta ] || [ -f ../.meta ])) && return
     [ "$(cat .meta | head -1)" = 'dir=>DAY' ] && cd ..
-    n="$(ls -1 | grep -E '^\d{2}$' | sort | tail -1)"
-    n=$((n+1))
+    n=$(ls -1 | grep -E '^\d{2}$' | sort | tail -1 || echo "00")
+    n=$((n + 1))
     aoc -d $n
     cd "$(printf "%02d" $n)"
     cp ../../template.py ./main.py
