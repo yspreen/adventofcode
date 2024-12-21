dbfc60e671ce1832d57d397f698a2c4fdcd5f628
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2024-12-21 17:15:42 -0500

    Add day 18

---
+++ 2024/18/main.py
@@ -0,0 +1,110 @@
+import numpy as np
+import re
+import pathlib
+import json
+from functools import reduce, cmp_to_key
+from string import ascii_lowercase
+from math import prod, gcd, sqrt
+from itertools import permutations, product
+from llist import dllist as llist
+from copy import deepcopy
+from hashlib import md5, sha256
+from os import environ
+
+
+def read():
+    with open(DIR / "input") as f:
+        s = (f.read() if teststr == "" else teststr).splitlines()
+    return lmap(lambda r: lmap(int, r.split(",")), s)
+
+
+def maybeint(line):
+    try:
+        return int(line)
+    except:
+        return line
+
+
+mv = [
+    (-1, 0),  # U
+    (1, 0),  # D
+    (0, -1),  # L
+    (0, 1),  # R
+]
+mv_3d = [
+    (-1, 0, 0),  # U
+    (1, 0, 0),  # D
+    (0, -1, 0),  # L
+    (0, 1, 0),  # R
+    (0, 0, -1),  # B
+    (0, 0, 1),  # F
+]
+
+
+def BFS(start, can_walk, goal, cost_fn=None):
+    cost_fn = (lambda _, __: 1) if cost_fn is None else cost_fn
+
+    options = [(start, 0)]
+    visited = set([start])
+
+    while options:
+        new_o = []
+        for pos, cost in options:
+            for d in mv:
+                new_p = (pos[0] + d[0], pos[1] + d[1])
+                if new_p[0] < 0:  # lower bound check
+                    continue
+                if new_p[1] < 0:  # lower bound check
+                    continue
+                try:
+                    assert can_walk(pos, new_p)
+                except:
+                    continue  # upper bound check
+                if new_p in visited:
+                    continue
+                visited.add(new_p)
+                cost_ = cost + cost_fn(pos, new_p)
+                new_o.append((new_p, cost_))
+                if goal(new_p):
+                    return cost_
+        options = new_o
+    return None
+
+
+def easy():
+    max_x = max([x for x, y in t])
+    max_y = max([y for x, y in t])
+    A = np.zeros((max_x + 1, max_y + 1))
+
+    for x, y in t[:1024]:
+        A[x, y] = 1
+
+    print(BFS((0, 0), lambda a, b: A[b[0], b[1]] == 0, lambda a: a == (70, 70)))
+
+
+def hard():
+    max_x = max([x for x, y in t])
+    max_y = max([y for x, y in t])
+    A = np.zeros((max_x + 1, max_y + 1))
+
+    for i, xy in enumerate(t):
+        x, y = xy
+        A[x, y] = 1
+        if (
+            i >= 1024
+            and BFS((0, 0), lambda a, b: A[b[0], b[1]] == 0, lambda a: a == (70, 70))
+            is None
+        ):
+            return print(f"{x},{y}")
+
+
+teststr = """"""
+if environ.get("AOC_SOLVE", "") == "1":
+    teststr = ""
+DIR = pathlib.Path(__file__).parent.absolute()
+lmap = lambda *a: list(map(*a))
+inf = float("inf")
+t = read()
+if __name__ == "__main__":
+    easy()
+    hard()
