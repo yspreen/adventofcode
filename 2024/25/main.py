067509e26c6b81899ecd53e539fdba97e2b316a4
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2024-12-25 22:23:06 -0500

    Add day 25.

---
+++ 2024/25/main.py
@@ -0,0 +1,108 @@
+import numpy as np
+import re
+import pathlib
+import json
+from functools import reduce, cmp_to_key
+from string import ascii_lowercase
+from math import prod, gcd, sqrt
+from itertools import permutations, product
+from llist import dllist as llist
+from copy import deepcopy
+from hashlib import md5, sha256
+from os import environ
+
+
+def read():
+    with open(DIR / "input") as f:
+        s = (
+            (f.read() if teststr == "" else teststr)
+            .replace(".", "0")
+            .replace("#", "1")
+            .split("\n\n")
+        )
+    s = [np.array([[int(c) for c in r] for r in block.splitlines()]) for block in s]
+    locks = [a for a in s if a[0, 0] == 1]
+    keys = [a for a in s if a[-1, 0] == 1]
+    return locks, keys
+
+
+def maybeint(line):
+    try:
+        return int(line)
+    except:
+        return line
+
+
+mv = [
+    (-1, 0),  # U
+    (1, 0),  # D
+    (0, -1),  # L
+    (0, 1),  # R
+    (-1, 1),  # UR
+    (1, 1),  # DR
+    (-1, -1),  # UL
+    (1, -1),  # DL
+]
+mv_3d = [
+    (-1, 0, 0),  # U
+    (1, 0, 0),  # D
+    (0, -1, 0),  # L
+    (0, 1, 0),  # R
+    (0, 0, -1),  # B
+    (0, 0, 1),  # F
+]
+
+
+def BFS(start, can_walk, goal, cost_fn=None):
+    cost_fn = (lambda _, __: 1) if cost_fn is None else cost_fn
+
+    options = [(start, 0)]
+    visited = set([start])
+
+    while options:
+        new_o = []
+        for pos, cost in options:
+            for d in mv:
+                new_p = (pos[0] + d[0], pos[1] + d[1])
+                if new_p[0] < 0:  # lower bound check
+                    continue
+                if new_p[1] < 0:  # lower bound check
+                    continue
+                try:
+                    assert can_walk(pos, new_p)
+                except:
+                    continue  # upper bound check
+                if new_p in visited:
+                    continue
+                visited.add(new_p)
+                cost_ = cost + cost_fn(pos, new_p)
+                new_o.append((new_p, cost_))
+                if goal(new_p):
+                    return cost_
+        options = new_o
+    return None
+
+
+def easy():
+    result = 0
+    for lock in t[0]:
+        for key in t[1]:
+            if np.max(lock + key) == 1:
+                result += 1
+    print(result)
+
+
+def hard():
+    return
+
+
+teststr = """"""
+if environ.get("AOC_SOLVE", "") == "1":
+    teststr = ""
+DIR = pathlib.Path(__file__).parent.absolute()
+lmap = lambda *a: list(map(*a))
+inf = float("inf")
+t = read()
+if __name__ == "__main__":
+    easy()
+    hard()
