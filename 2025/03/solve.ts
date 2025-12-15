e901e0a2a2ba73d88ce6fa249a956587a3c73061
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2025-12-15 18:41:25 -0500

    Add day 3.

--- 2025/03/solve.ts
+++ 2025/03/solve.ts
@@ -1,6 +1,36 @@
-const input = await Bun.file("input").text();
+let input = await Bun.file("input").text();
 const lines = input.trim().split("\n");
 
-console.log("123");
+let answer = 0;
+
+for (const line of lines) {
+    let maxFirst = Math.max(...line.slice(0, line.length - 1).split('').map(v => +v));
+    let firstIndex = line.length;
+    for (let i = 0; i < firstIndex; i++) {
+        if (+line[i] === maxFirst) firstIndex = i
+    }
+    let maxSecond = Math.max(...line.slice(firstIndex + 1).split('').map(v => +v));
+    answer += +`${maxFirst}${maxSecond}`
+}
+
+console.log(answer)
+answer = 0;
+
+for (const line of lines) {
+    let lowerLimit = 0;
+    let s = '';
+    for (let k = 1; k <= 12; k++) {
+        let maxNum = Math.max(...line.slice(lowerLimit, line.length - (12 - k)).split('').map(v => +v));
+        let idx = line.length;
+        for (let i = lowerLimit; i < idx; i++) {
+            if (+line[i] === maxNum) idx = i
+        }
+        lowerLimit = idx + 1;
+        s += `${maxNum}`
+    }
+    answer += +s;
+}
+
+console.log(answer)
 
 export { }
