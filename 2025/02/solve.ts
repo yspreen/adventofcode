4e21223933430ef5d6e26f5f8de45ada7f2807d1
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2025-12-15 18:20:44 -0500

    Add day 2.

--- 2025/02/solve.ts
+++ 2025/02/solve.ts
@@ -1,6 +1,40 @@
 const input = await Bun.file("input").text();
-const lines = input.trim().split("\n");
+const ranges = input.trim().split(",");
 
-console.log("123");
+let invalid = 0;
+for (const range of ranges) {
+    const [lhs, rhs] = range.split("-").map(v => +v);
+    for (let i = lhs; i <= rhs; i++) {
+        const s = `${i}`;
+        if (s.length % 2) continue;
+        if (s.slice(0, s.length / 2) !== s.slice(s.length / 2)) continue;
+        invalid += i
+    }
+}
+
+console.log(invalid)
+
+invalid = 0;
+for (const range of ranges) {
+    const [lhs, rhs] = range.split("-").map(v => +v);
+    for (let i = lhs; i <= rhs; i++) {
+        const s = `${i}`;
+        for (let l = 1; l <= s.length / 2; l++) {
+            if (s.length % l) continue;
+            let foundWrong = false;
+            for (let j = l; j < s.length; j += l) {
+                if (s.slice(0, l) !== s.slice(j, j + l)) {
+                    foundWrong = true
+                    break
+                }
+            }
+            if (foundWrong) continue;
+            invalid += i
+            break;
+        }
+    }
+}
+
+console.log(invalid)
 
 export { }
