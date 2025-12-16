f2a8d7651bf5bd7416e43883ebc709c3f3153494
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2025-12-15 19:47:08 -0500

    Add day 5.

--- 2025/05/solve.ts
+++ 2025/05/solve.ts
@@ -1,6 +1,38 @@
-const input = await Bun.file("input").text();
-const lines = input.trim().split("\n");
+let input = await Bun.file("input").text();
+if (0) input = `3-5
+10-14
+16-20
+12-18
 
-console.log("123");
+1`
+const [ranges_, ids] = input.trim().split("\n\n").map(block => block.split("\n"));
+
+const ranges = ranges_.map(line => line.split('-').map(n => +n) as [number, number])
+
+let c = 0;
+for (const id of ids.map(id => +id)) {
+    for (const [lower, upper] of ranges) {
+        if (lower <= id && id <= upper) {
+            c += 1
+            break
+        }
+    }
+}
+
+console.log(c)
+
+c = 0;
+for (let i = 0; i < ranges.length; i++) {
+    const [iLower, iUpper] = ranges[i]
+    c += iUpper - iLower + 1
+    for (let j = i + 1; j < ranges.length; j++) {
+        const [jLower, jUpper] = ranges[j];
+        const lower = Math.max(iLower, jLower);
+        const upper = Math.min(iUpper, jUpper);
+        if (lower <= upper) c -= (upper - lower + 1)
+    }
+}
+
+console.log(c)
 
 export { }
