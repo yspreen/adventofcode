b3fc37a4946d58da7eaa37be145cdd298b4d72cf
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2025-12-24 00:52:33 +0100

    Add day 12.

--- 2025/12/solve.ts
+++ 2025/12/solve.ts
@@ -1,104 +1,5 @@
 import { readFile } from "fs/promises";
 
-const pieceOrientations: Piece[][] = [];
-
-type Piece = number[][];
-
-function rotate(piece: Piece) {
-  const zeros = piece.map((row) => row.map((_) => 0));
-  piece.forEach((row, i) => {
-    row.forEach((elem, j) => {
-      zeros[j][piece.length - i - 1] = elem;
-    });
-  });
-  return zeros;
-}
-function flip(piece: Piece) {
-  const zeros = piece[0].map(() => piece.map((_) => 0));
-  piece.forEach((row, i) => {
-    row.forEach((elem, j) => {
-      zeros[j][i] = elem;
-    });
-  });
-  return rotate(zeros);
-}
-
-function contains(pieces: Piece[], piece: Piece) {
-  const hashes = pieces.map((p) => p.join("."));
-  return hashes.includes(piece.join("."));
-}
-
-function fits(grid: number[][], piece: Piece, x: number, y: number) {
-  for (let i = 0; i < 3; i++)
-    for (let j = 0; j < 3; j++)
-      if (piece[i][j] && grid[x + i][y + j]) return false;
-  return true;
-}
-
-function allZero(numbers: number[]) {
-  for (const number of numbers) {
-    if (number !== 0) return false;
-  }
-  return true;
-}
-
-function place(grid: number[][], piece: Piece, x: number, y: number) {
-  for (let i = 0; i < 3; i++)
-    for (let j = 0; j < 3; j++) if (piece[i][j]) grid[x + i][y + j] += 1;
-}
-function unPlace(grid: number[][], piece: Piece, x: number, y: number) {
-  for (let i = 0; i < 3; i++)
-    for (let j = 0; j < 3; j++) if (piece[i][j]) grid[x + i][y + j] -= 1;
-}
-
-// function piecesFit(grid: number[][], counts: number[], x: number, y: number) {
-//   console.log(grid.join("\n"));
-//   console.log(counts.join(","));
-//   console.log(x, y);
-//   const result = piecesFit_(grid, counts, x, y);
-//   console.log(result);
-//   console.log();
-//   return result;
-// }
-function piecesFit(
-  grid: number[][],
-  counts: number[],
-  x: number,
-  y: number,
-  emptyChoices = 0,
-): boolean {
-  if (x === grid.length - 2) return false;
-  if (allZero(counts)) return true;
-
-  let nextX = x;
-  let nextY = y + 1;
-  if (nextY === grid[0].length - 2) {
-    nextX += 1;
-    nextY = 0;
-  }
-
-  for (let i = 0; i < counts.length; i++) {
-    const count = counts[i];
-    if (count == 0) continue;
-    counts[i] -= 1;
-    for (const piece of pieceOrientations[i]) {
-      if (!fits(grid, piece, x, y)) continue;
-      place(grid, piece, x, y);
-      if (piecesFit(grid, counts, nextX, nextY)) return true;
-      unPlace(grid, piece, x, y);
-    }
-    counts[i] += 1;
-  }
-
-  return piecesFit(
-    grid,
-    counts,
-    nextX,
-    nextY,
-    nextY === 0 ? 0 : emptyChoices + 1,
-  );
-}
-
 async function main() {
   let input = await readFile("input", "utf-8");
   if (0)
@@ -144,51 +45,30 @@ async function main() {
 
   const grids = chunks.pop()!;
 
-  let i = 0;
-  for (let piece of chunks) {
-    piece = piece.replace(/\n/g, "");
-    piece = piece.split(":")[1]!;
-    piece = piece.replace(/\#/g, "1");
-    piece = piece.replace(/\./g, "0");
-    piece = piece.replace(/(...)/g, "$1\n");
-    let onePiece = piece
-      .trim()
-      .split("\n")
-      .map((p) => p.split("").map((v) => +v));
-
-    pieceOrientations.push([]);
-
-    for (const _ of "1234") {
-      void _;
-      onePiece = rotate(onePiece);
-      if (!contains(pieceOrientations[i], onePiece)) {
-        pieceOrientations[i].push(onePiece);
-      }
-      onePiece = flip(onePiece);
-      if (!contains(pieceOrientations[i], onePiece)) {
-        pieceOrientations[i].push(onePiece);
-      }
-      onePiece = flip(onePiece);
+  const blockCounts: number[] = [];
+  for (const piece of chunks) {
+    blockCounts.push(0);
+    for (const c of piece.replace(/\n/g, "").split(":")[1]) {
+      if (c === "#") blockCounts[blockCounts.length - 1]++;
     }
-    i++;
   }
 
+  let a = 0;
   for (const gridString of grids.trim().split("\n")) {
-    console.log(gridString);
     const dimsString = gridString.split(":")[0];
     const counts = gridString
       .split(" ")
       .slice(1)
       .map((v) => +v);
     const dims = dimsString.split("x").map((v) => +v) as [number, number];
-    const grid: number[][] = [];
-    new Array(dims[1]).fill(0).forEach((_) => {
-      grid.push(new Array(dims[0]).fill(0).map((_) => 0));
-    });
-    console.log(piecesFit(grid, counts, 0, 0));
-    console.log(grid.join("\n"));
-    //   console.log(rotate(rotate(rotate(flip(grid)))).join("\n"));
+
+    const gridSize = dims[0] * dims[1];
+    const blockTotal = counts
+      .map((c, i) => c * blockCounts[i])
+      .reduce((a, b) => a + b);
+    a += blockTotal <= gridSize ? 1 : 0;
   }
+  console.log(a);
 }
 
 main().catch(console.error);
